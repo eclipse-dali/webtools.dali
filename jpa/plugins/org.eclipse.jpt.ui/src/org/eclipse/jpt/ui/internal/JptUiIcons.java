f60844e9ab6fb7dc259453fee16030f2b73e6ac3
nhauge <nhauge>
2007-04-26 05:10:57 +0000

    This commit was manufactured by cvs2svn to create tag 'v200703070000'.

--- jpa/plugins/org.eclipse.jpt.ui/src/org/eclipse/jpt/ui/internal/JptUiIcons.java
+++
@@ -1,6 +0,0 @@
-package org.eclipse.jpt.ui.internal;
-
-public class JptUiIcons
-{
-	public static final String JPA_WIZ_BANNER = "full/wizban/jpa_facet_wizban"; //$NON-NLS-1$
-}
--- jpa/plugins/org.eclipse.jpt.ui/src/org/eclipse/jpt/ui/internal/generic/GenericPlatformUi.java
+++
@@ -1,6 +0,0 @@
-package org.eclipse.jpt.ui.internal.generic;
-
-import org.eclipse.jpt.ui.internal.BaseJpaPlatformUi;
-
-public class GenericPlatformUi extends BaseJpaPlatformUi
-{}
