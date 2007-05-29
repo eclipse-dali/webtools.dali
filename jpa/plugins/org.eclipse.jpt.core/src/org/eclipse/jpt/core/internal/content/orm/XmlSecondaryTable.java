1056129e8fcdc046e3321b39c1088f96f05509b9
kmoore <kmoore>
2007-05-29 19:26:20 +0000

    188979 -secondary table pkJoinColumn defaults not appearing in orm.xml

--- jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/internal/content/orm/XmlSecondaryTable.java
+++ jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/internal/content/orm/XmlSecondaryTable.java
@@ -60,6 +60,7 @@ public class XmlSecondaryTable extends AbstractXmlTable
 
 	protected XmlSecondaryTable(Owner owner) {
 		super(owner);
+		this.getDefaultPrimaryKeyJoinColumns().add(this.createPrimaryKeyJoinColumn(0));
 	}
 
 	/**
