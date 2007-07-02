9add9d2d417dec9c053a33922419d986eaa756aa
kmoore <kmoore>
2007-07-02 22:38:13 +0000

    192717 - Version attribute mapping in orm.xml doesn't update

--- jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/internal/content/orm/resource/VersionTranslator.java
+++ jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/internal/content/orm/resource/VersionTranslator.java
@@ -10,18 +10,34 @@
 package org.eclipse.jpt.core.internal.content.orm.resource;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jpt.core.internal.mappings.IBasic;
+import org.eclipse.jpt.core.internal.mappings.IVersion;
+import org.eclipse.jpt.core.internal.mappings.JpaCoreMappingsPackage;
 import org.eclipse.wst.common.internal.emf.resource.IDTranslator;
 import org.eclipse.wst.common.internal.emf.resource.Translator;
 
 public class VersionTranslator extends AttributeMappingTranslator 
 {
+	private ColumnTranslator columnTranslator;
+
 	public VersionTranslator() {
 		super(VERSION, NO_STYLE);
+		this.columnTranslator = createColumnTranslator();
+	}
+	
+	private ColumnTranslator createColumnTranslator() {
+		return new ColumnTranslator(COLUMN, JPA_CORE_XML_PKG.getIXmlColumnMapping_ColumnForXml());
 	}
 	
 	@Override
 	public EObject createEMFObject(String nodeName, String readAheadName) {
-		return JPA_CORE_XML_FACTORY.createXmlVersion();
+		IVersion version = JPA_CORE_XML_FACTORY.createXmlVersion();
+		setVersion(version);
+		return version;
+	}
+	
+	protected void setVersion(IVersion version) {
+		this.columnTranslator.setColumnMapping(version);		
 	}
 	
 	@Override
@@ -29,8 +45,13 @@ public class VersionTranslator extends AttributeMappingTranslator
 		return new Translator[] {
 			IDTranslator.INSTANCE,
 			createNameTranslator(),
+			columnTranslator, 
+			createTemporalTranslator(),
 		};
 	}
 
+	private Translator createTemporalTranslator() {
+		return new TemporalTypeElementTranslator(TEMPORAL, JpaCoreMappingsPackage.eINSTANCE.getIVersion_Temporal(), NO_STYLE);
+	}
 
 }
