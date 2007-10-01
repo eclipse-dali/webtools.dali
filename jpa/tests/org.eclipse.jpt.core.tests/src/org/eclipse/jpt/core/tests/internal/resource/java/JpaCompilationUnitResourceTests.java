5671d3e07b6772d2d4f9a23a1e4b284a580d8d38
kmoore <kmoore>
2007-10-01 15:49:58 +0000

    first pass at implementing JavaResourceModel

--- jpa/tests/org.eclipse.jpt.core.tests/src/org/eclipse/jpt/core/tests/internal/resource/java/JpaCompilationUnitResourceTests.java
+++ jpa/tests/org.eclipse.jpt.core.tests/src/org/eclipse/jpt/core/tests/internal/resource/java/JpaCompilationUnitResourceTests.java
@@ -10,15 +10,23 @@
 package org.eclipse.jpt.core.tests.internal.resource.java;
 
 import java.util.Iterator;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IType;
+import org.eclipse.jpt.core.internal.IJpaFile;
 import org.eclipse.jpt.core.internal.IJpaPlatform;
-import org.eclipse.jpt.core.internal.jdtutility.JDTTools;
+import org.eclipse.jpt.core.internal.IJpaProject;
 import org.eclipse.jpt.core.internal.resource.java.Entity;
 import org.eclipse.jpt.core.internal.resource.java.GenericJpaPlatform;
 import org.eclipse.jpt.core.internal.resource.java.JPA;
+import org.eclipse.jpt.core.internal.resource.java.JavaResourceModel;
 import org.eclipse.jpt.core.internal.resource.java.JpaCompilationUnitResource;
 import org.eclipse.jpt.core.tests.internal.jdtutility.AnnotationTestCase;
+import org.eclipse.jpt.core.tests.internal.projects.TestJavaProject;
+import org.eclipse.jpt.core.tests.internal.projects.TestJpaProject;
+import org.eclipse.jpt.utility.internal.ClassTools;
 import org.eclipse.jpt.utility.internal.iterators.ArrayIterator;
 
 public class JpaCompilationUnitResourceTests extends AnnotationTestCase {
@@ -27,6 +35,11 @@ public class JpaCompilationUnitResourceTests extends AnnotationTestCase {
 		super(name);
 	}
 
+	@Override
+	protected TestJavaProject buildJavaProject(String projectName, boolean autoBuild) throws Exception {
+		return new TestJpaProject(projectName, autoBuild);  // false = no auto-build
+	}
+	
 	private void createAnnotationAndMembers(String annotationName, String annotationBody) throws Exception {
 		this.javaProject.createType("javax.persistence", annotationName + ".java", "public @interface " + annotationName + " { " + annotationBody + " }");
 	}
@@ -56,16 +69,16 @@ public class JpaCompilationUnitResourceTests extends AnnotationTestCase {
 		return new GenericJpaPlatform();
 	}
 	
-	protected JpaCompilationUnitResource buildJpaCompilationUnit(ICompilationUnit testCompilationUnit) {
-		JpaCompilationUnitResource compilationUnit = 
-			new JpaCompilationUnitResource(testCompilationUnit, buildJpaPlatform());
-		compilationUnit.updateFromJava(JDTTools.buildASTRoot(testCompilationUnit));
-		return compilationUnit;
+	protected JpaCompilationUnitResource getJpaCompilationUnitResource(ICompilationUnit testCompilationUnit) throws CoreException {
+		IJpaProject jpaProject = ((TestJpaProject) this.javaProject).getJpaProject();
+		IJpaFile jpaFile = jpaProject.getJpaFile((IFile) testCompilationUnit.getResource());
+		JavaResourceModel javaResourceModel = (JavaResourceModel) jpaFile.getResourceModel();
+		return (JpaCompilationUnitResource) ClassTools.getFieldValue(javaResourceModel, "compilationUnitResource");
 	}
 	
 	public void testGetPersistentType() throws Exception {
 		ICompilationUnit compilationUnit = this.createTestCompilationUnit();
-		JpaCompilationUnitResource jpaCompilationUnit = buildJpaCompilationUnit(compilationUnit);
+		JpaCompilationUnitResource jpaCompilationUnit = getJpaCompilationUnitResource(compilationUnit);
 		
 		assertTrue(jpaCompilationUnit.getPersistentType().mappingAnnotation() instanceof Entity);
 	}
