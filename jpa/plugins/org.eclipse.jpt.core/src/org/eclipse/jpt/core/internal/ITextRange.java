6fb00f6f54b3dac475714b492c1a6bfeac961942
kmoore <kmoore>
2007-10-22 13:49:33 +0000

    made static instance private, already a public instance() method for the singleton

--- jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/internal/ITextRange.java
+++ jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/internal/ITextRange.java
@@ -61,7 +61,7 @@ public interface ITextRange {
 	 * Empty implementation of text range.
 	 */
 	final class Empty implements ITextRange {
-		public static final ITextRange INSTANCE = new Empty();
+		private static final ITextRange INSTANCE = new Empty();
 		public static ITextRange instance() {
 			return INSTANCE;
 		}
