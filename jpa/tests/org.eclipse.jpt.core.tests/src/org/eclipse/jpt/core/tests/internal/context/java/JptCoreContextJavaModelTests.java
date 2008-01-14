4ff5c1161762e7c2986171a2c2f10e754bb72bc3
kmoore <kmoore>
2008-01-14 17:00:27 +0000

    uncommented test code

--- jpa/tests/org.eclipse.jpt.core.tests/src/org/eclipse/jpt/core/tests/internal/context/java/JptCoreContextJavaModelTests.java
+++ jpa/tests/org.eclipse.jpt.core.tests/src/org/eclipse/jpt/core/tests/internal/context/java/JptCoreContextJavaModelTests.java
@@ -22,34 +22,34 @@ public class JptCoreContextJavaModelTests extends TestCase
 
 	public static Test suite(boolean all) {
 		TestSuite suite = new TestSuite(JptCoreContextJavaModelTests.class.getName());
-//		suite.addTestSuite(JavaAttributeOverrideTests.class);
-//		suite.addTestSuite(JavaBasicMappingTests.class);
-//		suite.addTestSuite(JavaColumnTests.class);
-//		suite.addTestSuite(JavaDiscriminatorColumnTests.class);
-//		suite.addTestSuite(JavaEmbeddableTests.class);
-//		suite.addTestSuite(JavaEmbeddedIdMappingTests.class);
-//		suite.addTestSuite(JavaEmbeddedMappingTests.class);
+		suite.addTestSuite(JavaAttributeOverrideTests.class);
+		suite.addTestSuite(JavaBasicMappingTests.class);
+		suite.addTestSuite(JavaColumnTests.class);
+		suite.addTestSuite(JavaDiscriminatorColumnTests.class);
+		suite.addTestSuite(JavaEmbeddableTests.class);
+		suite.addTestSuite(JavaEmbeddedIdMappingTests.class);
+		suite.addTestSuite(JavaEmbeddedMappingTests.class);
 		suite.addTestSuite(JavaEntityTests.class);
-//		suite.addTestSuite(JavaGeneratedValueTests.class);
-//		suite.addTestSuite(JavaIdMappingTests.class);
-//		suite.addTestSuite(JavaJoinTableTests.class);		
+		suite.addTestSuite(JavaGeneratedValueTests.class);
+		suite.addTestSuite(JavaIdMappingTests.class);
+		suite.addTestSuite(JavaJoinTableTests.class);		
 		suite.addTestSuite(JavaMappedSuperclassTests.class);
-//		suite.addTestSuite(JavaManyToManyMappingTests.class);
-//		suite.addTestSuite(JavaManyToOneMappingTests.class);
+		suite.addTestSuite(JavaManyToManyMappingTests.class);
+		suite.addTestSuite(JavaManyToOneMappingTests.class);
 		suite.addTestSuite(JavaNamedQueryTests.class);
 		suite.addTestSuite(JavaNamedNativeQueryTests.class);
 		suite.addTestSuite(JavaQueryHintTests.class);
-//		suite.addTestSuite(JavaOneToManyMappingTests.class);
-//		suite.addTestSuite(JavaOneToOneMappingTests.class);
-//		suite.addTestSuite(JavaPersistentTypeTests.class);
-//		suite.addTestSuite(JavaPersistentAttributeTests.class);
-//		suite.addTestSuite(JavaPrimaryKeyJoinColumnTests.class);
-//		suite.addTestSuite(JavaSecondaryTableTests.class);
-//		suite.addTestSuite(JavaSequenceGeneratorTests.class);
-//		suite.addTestSuite(JavaTableGeneratorTests.class);
-//		suite.addTestSuite(JavaTableTests.class);
-//		suite.addTestSuite(JavaTransientMappingTests.class);
-//		suite.addTestSuite(JavaVersionMappingTests.class);
+		suite.addTestSuite(JavaOneToManyMappingTests.class);
+		suite.addTestSuite(JavaOneToOneMappingTests.class);
+		suite.addTestSuite(JavaPersistentTypeTests.class);
+		suite.addTestSuite(JavaPersistentAttributeTests.class);
+		suite.addTestSuite(JavaPrimaryKeyJoinColumnTests.class);
+		suite.addTestSuite(JavaSecondaryTableTests.class);
+		suite.addTestSuite(JavaSequenceGeneratorTests.class);
+		suite.addTestSuite(JavaTableGeneratorTests.class);
+		suite.addTestSuite(JavaTableTests.class);
+		suite.addTestSuite(JavaTransientMappingTests.class);
+		suite.addTestSuite(JavaVersionMappingTests.class);
 		return suite;
 	}
 
