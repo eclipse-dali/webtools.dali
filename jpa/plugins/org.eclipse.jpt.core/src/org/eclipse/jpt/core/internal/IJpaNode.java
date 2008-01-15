0f01f53d2e1f3936861a3d1bc20add3d6440f64e
bvosburgh <bvosburgh>
2008-01-15 14:23:35 +0000

    tweaked comments

--- jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/internal/IJpaNode.java
+++ jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/internal/IJpaNode.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007 Oracle. All rights reserved.
+ * Copyright (c) 2007, 2008 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -20,20 +20,21 @@ public interface IJpaNode extends Node, IAdaptable
 {
 
 	/**
-	 * Return the JPA project the object belongs to.
+	 * Return the JPA project the node belongs to.
 	 */
 	IJpaProject jpaProject();
 
 	/**
-	 * Return the resource that most directly contains the object.
+	 * Return the resource that most directly contains the node.
 	 * This is used by JpaHelper.
 	 */
 	IResource resource();
-	
-	
+
+
 	// ********** covariant overrides **********
 
 	IJpaNode parent();
 
 	IJpaProject root();
+
 }
