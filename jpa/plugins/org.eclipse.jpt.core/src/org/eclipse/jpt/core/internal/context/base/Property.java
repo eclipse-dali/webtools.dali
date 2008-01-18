1672eb9099afbb1cb1b956cf8e4f58392a946dd5
tle <tle>
2008-01-18 02:29:49 +0000

    Added toString.

--- jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/internal/context/base/Property.java
+++ jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/internal/context/base/Property.java
@@ -1,12 +1,11 @@
 /*******************************************************************************
- *  Copyright (c) 2007 Oracle. 
- *  All rights reserved.  This program and the accompanying materials 
- *  are made available under the terms of the Eclipse Public License v1.0 
- *  which accompanies this distribution, and is available at 
- *  http://www.eclipse.org/legal/epl-v10.html
- *  
- *  Contributors: 
- *  	Oracle - initial API and implementation
+ * Copyright (c) 2007, 2008 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0, which accompanies this distribution
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ * 
+ * Contributors:
+ *     Oracle - initial API and implementation
  *******************************************************************************/
 package org.eclipse.jpt.core.internal.context.base;
 
@@ -62,4 +61,17 @@ public class Property extends JpaContextNode
 		setName(property.getName());
 		setValue(property.getValue());
 	}
+	
+	
+	// **************** toString
+	@Override
+	public void toString(StringBuilder sb)
+	{
+		sb.append(" (name: ");
+		sb.append(name);
+		sb.append(", value: ");
+		sb.append(value);
+		sb.append(')');
+	}
+
 }
