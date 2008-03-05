78fa1492677448fa6c6749bcf3603540c8b4cf75
kmoore <kmoore>
2008-03-05 21:34:01 +0000

    SuppressWarnings nls - from Pascal

--- jpa/plugins/org.eclipse.jpt.ui/src/org/eclipse/jpt/ui/JptUiPlugin.java
+++ jpa/plugins/org.eclipse.jpt.ui/src/org/eclipse/jpt/ui/JptUiPlugin.java
@@ -3,7 +3,7 @@
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
- * 
+ *
  * Contributors:
  *     Oracle - initial API and implementation
  ******************************************************************************/
@@ -18,46 +18,47 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
 /**
- * 
- * 
+ *
+ *
  * Provisional API: This interface is part of an interim API that is still
  * under development and expected to change significantly before reaching
  * stability. It is available at this early stage to solicit feedback from
  * pioneering adopters on the understanding that any code that uses this API
  * will almost certainly be broken (repeatedly) as the API evolves.
  */
+@SuppressWarnings("nls")
 public class JptUiPlugin extends AbstractUIPlugin
 {
 	private static JptUiPlugin INSTANCE;
-	
+
 	/**
 	 * The plug-in identifier of JPA UI support
 	 * (value <code>"org.eclipse.jpt.ui"</code>).
 	 */
 	public final static String PLUGIN_ID = "org.eclipse.jpt.ui";  //$NON-NLS-1$
-	
+
 	/**
 	 * Returns the singleton Plugin
 	 */
 	public static JptUiPlugin getPlugin() {
 		return INSTANCE;
 	}
-	
+
 	public static void log(IStatus status) {
         INSTANCE.getLog().log(status);
     }
-	
+
 	public static void log(String msg) {
         log(new Status(IStatus.ERROR, PLUGIN_ID, IStatus.OK, msg, null));
     }
-	
+
 	public static void log(Throwable throwable) {
 		log(new Status(IStatus.ERROR, PLUGIN_ID, IStatus.OK, throwable.getLocalizedMessage(), throwable));
 	}
-	
-	
+
+
 	// **************** Image API **********************************************
-	
+
 	/**
 	 * This gets a .gif from the icons folder.
 	 */
@@ -70,7 +71,7 @@ public class JptUiPlugin extends AbstractUIPlugin
 		}
 		return imageDescriptorFromPlugin(PLUGIN_ID, key);
 	}
-	
+
 	/**
 	 * This returns an image for a .gif from the icons folder
 	 */
@@ -78,16 +79,16 @@ public class JptUiPlugin extends AbstractUIPlugin
 		ImageDescriptor desc = getImageDescriptor(key);
 		return (desc == null) ? null : desc.createImage();
 	}
-	
-	
+
+
 	// **************** Construction *******************************************
-	
+
 	public JptUiPlugin() {
 		super();
 		INSTANCE = this;
 	}
-	
-	
+
+
 	/**
 	 * Return the JPA platform UI corresponding to the given JPA platform
 	 */
