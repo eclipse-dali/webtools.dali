5066738e8ebae9c154246ab29a054059ae9259e8
tle <tle>
2008-04-02 16:39:02 +0000

    Initial checkin.

---
+++ jpa/tests/org.eclipse.jpt.eclipselink.core.tests/src/org/eclipse/jpt/eclipselink/core/tests/internal/customization/JptEclipseLinkCoreCustomizationTests.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0, which accompanies this distribution
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ * 
+ * Contributors:
+ *     Oracle - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.jpt.eclipselink.core.tests.internal.customization;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * decentralize test creation code
+ */
+public class JptEclipseLinkCoreCustomizationTests
+{
+	public static Test suite() {
+		TestSuite suite = new TestSuite(JptEclipseLinkCoreCustomizationTests.class.getPackage().getName());
+		
+		suite.addTestSuite(CustomizationValueModelTests.class);
+		suite.addTestSuite(CustomizationAdapterTests.class);
+		
+		return suite;
+	}
+
+	private JptEclipseLinkCoreCustomizationTests() {
+		super();
+		throw new UnsupportedOperationException();
+	}
+}
