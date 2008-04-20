1fa1c42bdaa4f4397a2ae3bdaab5259fe63416dd
tle <tle>
2008-04-20 21:26:40 +0000

    Updated logger for handling custom class - patch from Pascal

--- jpa/plugins/org.eclipse.jpt.eclipselink.ui/src/org/eclipse/jpt/eclipselink/ui/internal/logging/LoggerComposite.java
+++ jpa/plugins/org.eclipse.jpt.eclipselink.ui/src/org/eclipse/jpt/eclipselink/ui/internal/logging/LoggerComposite.java
@@ -3,18 +3,31 @@
 * This program and the accompanying materials are made available under the
 * terms of the Eclipse Public License v1.0, which accompanies this distribution
 * and is available at http://www.eclipse.org/legal/epl-v10.html.
-* 
+*
 * Contributors:
 *     Oracle - initial API and implementation
 *******************************************************************************/
 package org.eclipse.jpt.eclipselink.ui.internal.logging;
 
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+
+import org.eclipse.jpt.eclipselink.core.internal.context.logging.Logger;
 import org.eclipse.jpt.eclipselink.core.internal.context.logging.Logging;
 import org.eclipse.jpt.eclipselink.ui.internal.EclipseLinkUiMessages;
 import org.eclipse.jpt.ui.internal.util.SWTUtil;
 import org.eclipse.jpt.ui.internal.widgets.AbstractPane;
+import org.eclipse.jpt.utility.internal.CollectionTools;
+import org.eclipse.jpt.utility.internal.StringConverter;
+import org.eclipse.jpt.utility.internal.iterators.TransformationIterator;
+import org.eclipse.jpt.utility.internal.model.value.CompositeListValueModel;
 import org.eclipse.jpt.utility.internal.model.value.PropertyAspectAdapter;
 import org.eclipse.jpt.utility.internal.model.value.PropertyListValueModelAdapter;
+import org.eclipse.jpt.utility.internal.model.value.SimpleCollectionValueModel;
+import org.eclipse.jpt.utility.internal.model.value.SortedListValueModelAdapter;
+import org.eclipse.jpt.utility.model.value.CollectionValueModel;
 import org.eclipse.jpt.utility.model.value.ListValueModel;
 import org.eclipse.jpt.utility.model.value.PropertyValueModel;
 import org.eclipse.jpt.utility.model.value.WritablePropertyValueModel;
@@ -55,6 +68,49 @@ public class LoggerComposite extends AbstractPane<Logging>
 		);
 	}
 
+	private String buildDisplayString(String loggerName) {
+
+		switch (Logger.valueOf(loggerName)) {
+			case default_logger: {
+				return EclipseLinkUiMessages.LoggerComposite_default_logger;
+			}
+			case java_logger: {
+				return EclipseLinkUiMessages.LoggerComposite_java_logger;
+			}
+			case server_logger: {
+				return EclipseLinkUiMessages.LoggerComposite_server_logger;
+			}
+			default: {
+				return null;
+			}
+		}
+	}
+
+	private Comparator<String> buildLoggerComparator() {
+		return new Comparator<String>() {
+			public int compare(String logger1, String logger2) {
+				logger1 = buildDisplayString(logger1);
+				logger2 = buildDisplayString(logger2);
+				return Collator.getInstance().compare(logger1, logger2);
+			}
+		};
+	}
+
+	private StringConverter<String> buildLoggerConverter() {
+		return new StringConverter<String>() {
+			public String convertToString(String value) {
+				try {
+					Logger.valueOf(value);
+					value = buildDisplayString(value);
+				}
+				catch (Exception e) {
+					// Ignore since the value is not a Logger
+				}
+				return value;
+			}
+		};
+	}
+
 	private WritablePropertyValueModel<String> buildLoggerHolder() {
 		return new PropertyAspectAdapter<Logging, String>(this.getSubjectHolder(), Logging.LOGGER_PROPERTY) {
 			@Override
@@ -76,6 +132,35 @@ public class LoggerComposite extends AbstractPane<Logging>
 				subject.setLogger(value);
 			}
 		};
+	}
+
+	private ListValueModel<String> buildLoggerListHolder() {
+		ArrayList<ListValueModel<String>> holders = new ArrayList<ListValueModel<String>>(2);
+		holders.add(buildDefaultLoggerListHolder());
+		holders.add(buildLoggersListHolder());
+		return new CompositeListValueModel<ListValueModel<String>, String>(holders);
+	}
+
+	private Iterator<String> buildLoggers() {
+		return new TransformationIterator<Logger, String>(CollectionTools.iterator(Logger.values())) {
+			@Override
+			protected String transform(Logger next) {
+				return next.name();
+			}
+		};
+	}
+
+	private CollectionValueModel<String> buildLoggersCollectionHolder() {
+		return new SimpleCollectionValueModel<String>(
+			CollectionTools.collection(buildLoggers())
+		);
+	}
+
+	private ListValueModel<String> buildLoggersListHolder() {
+		return new SortedListValueModelAdapter<String>(
+			buildLoggersCollectionHolder(),
+			buildLoggerComparator()
+		);
 	}
 
 	private String defaultValue(Logging subject) {
@@ -101,14 +186,12 @@ public class LoggerComposite extends AbstractPane<Logging>
 		CCombo combo = buildLabeledEditableCCombo(
 			container,
 			EclipseLinkUiMessages.PersistenceXmlLoggingTab_loggerLabel,
-			this.buildDefaultLoggerListHolder(),
+			this.buildLoggerListHolder(),
 			this.buildLoggerHolder(),
+			this.buildLoggerConverter(),
 			null		// EclipseLinkHelpContextIds.LOGGER_NAME
 		);
-		combo.add(EclipseLinkUiMessages.LoggerComposite_default_logger, 1);
-		combo.add(EclipseLinkUiMessages.LoggerComposite_java_logger, 2);
-		combo.add(EclipseLinkUiMessages.LoggerComposite_server_logger, 3);
-		
+
 		SWTUtil.attachDefaultValueHandler(combo);
 	}
 }
