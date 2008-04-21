69795c03dc313a21fe55086c933be01400c2ae78
kmoore <kmoore>
2008-04-21 15:37:55 +0000

    changed default cache size default to 1000 after talking with Shaun, the eclipselink docs were wrong

--- jpa/plugins/org.eclipse.jpt.eclipselink.core/src/org/eclipse/jpt/eclipselink/core/internal/context/caching/Caching.java
+++ jpa/plugins/org.eclipse.jpt.eclipselink.core/src/org/eclipse/jpt/eclipselink/core/internal/context/caching/Caching.java
@@ -32,7 +32,7 @@ public interface Caching extends PersistenceUnitProperties
 			static final String CACHE_SIZE_DEFAULT_PROPERTY = "cacheSizeDefaultProperty";
 			// EclipseLink key string
 			static final String ECLIPSELINK_CACHE_SIZE_DEFAULT = "eclipselink.cache.size.default";
-			static final Integer DEFAULT_CACHE_SIZE_DEFAULT = 100;
+			static final Integer DEFAULT_CACHE_SIZE_DEFAULT = new Integer(1000);
 
 	Boolean getDefaultSharedCacheDefault();
 	Boolean getSharedCacheDefault();
@@ -57,7 +57,7 @@ public interface Caching extends PersistenceUnitProperties
 			static final String CACHE_SIZE_PROPERTY = "cacheSizeProperty";
 			// EclipseLink key string
 			static final String ECLIPSELINK_CACHE_SIZE = "eclipselink.cache.size.";
-			static final Integer DEFAULT_CACHE_SIZE = 1000;
+			static final Integer DEFAULT_CACHE_SIZE = new Integer(1000);
 
 	Boolean getDefaultSharedCache();
 	Boolean getSharedCache(String entityName);
@@ -65,7 +65,7 @@ public interface Caching extends PersistenceUnitProperties
 			static final String SHARED_CACHE_PROPERTY = "sharedCacheProperty";
 			// EclipseLink key string
 			static final String ECLIPSELINK_SHARED_CACHE = "eclipselink.cache.shared.";
-			static final Boolean DEFAULT_SHARED_CACHE = true;
+			static final Boolean DEFAULT_SHARED_CACHE = Boolean.TRUE;
 
 
 	ListIterator<String> entities();
