82c07736948977cc7e73a44c10e8de58318c5cb7
kmoore <kmoore>
2008-09-26 12:08:40 +0000

    uncommented old failing test, want to see what happens in the build. reordered tests, oddly that makes them pass reliably on my machine, we'll see how it works for others

--- jpa/tests/org.eclipse.jpt.core.tests/src/org/eclipse/jpt/core/tests/internal/model/JpaModelTests.java
+++ jpa/tests/org.eclipse.jpt.core.tests/src/org/eclipse/jpt/core/tests/internal/model/JpaModelTests.java
@@ -9,6 +9,9 @@
  ******************************************************************************/
 package org.eclipse.jpt.core.tests.internal.model;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import junit.framework.TestCase;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
@@ -82,32 +85,12 @@ public class JpaModelTests extends TestCase {
 		return testProject;
 	}	
 
-	private IFile file(TestPlatformProject p, String path) {
+	private IFile getFile(TestPlatformProject p, String path) {
 		return p.getProject().getFile(new Path(path));
 	}
 
 	public void testJpaModel() {
 		assertNotNull(JptCorePlugin.getJpaModel());
-	}
-
-	public void testFacetInstallUninstall() throws Exception {
-		assertNull(JptCorePlugin.getJpaProject(this.testProject.getProject()));
-
-		this.testProject.installFacet("jpt.jpa", "1.0");
-		assertEquals(1, JptCorePlugin.getJpaModel().jpaProjectsSize());
-		JpaProject jpaProject = JptCorePlugin.getJpaProject(this.testProject.getProject());
-		assertNotNull(jpaProject);
-		assertEquals(4, jpaProject.jpaFilesSize());
-		assertNotNull(jpaProject.getJpaFile(this.file(this.testProject, "src/test/pkg/TestEntity.java")));
-		assertNotNull(jpaProject.getJpaFile(this.file(this.testProject, "src/test/pkg/TestEntity2.java")));
-
-		assertNotNull(jpaProject.getJpaFile(this.file(this.testProject, "src/META-INF/persistence.xml")));
-		assertNotNull(jpaProject.getJpaFile(this.file(this.testProject, "src/META-INF/orm.xml")));
-
-		this.testProject.uninstallFacet("jpt.jpa", "1.0");
-		assertEquals(0, JptCorePlugin.getJpaModel().jpaProjectsSize());
-		jpaProject = JptCorePlugin.getJpaProject(this.testProject.getProject());
-		assertNull(jpaProject);
 	}
 
 	public void testProjectCloseReopen() throws Exception {
@@ -123,11 +106,11 @@ public class JpaModelTests extends TestCase {
 		jpaProject = JptCorePlugin.getJpaProject(this.testProject.getProject());
 		assertNotNull(jpaProject);
 		assertEquals(4, jpaProject.jpaFilesSize());
-		assertNotNull(jpaProject.getJpaFile(this.file(this.testProject, "src/test/pkg/TestEntity.java")));
-		assertNotNull(jpaProject.getJpaFile(this.file(this.testProject, "src/test/pkg/TestEntity2.java")));
+		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/test/pkg/TestEntity.java")));
+		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/test/pkg/TestEntity2.java")));
 
-		assertNotNull(jpaProject.getJpaFile(this.file(this.testProject, "src/META-INF/persistence.xml")));
-		assertNotNull(jpaProject.getJpaFile(this.file(this.testProject, "src/META-INF/orm.xml")));
+		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/META-INF/persistence.xml")));
+		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/META-INF/orm.xml")));
 	}
 
 	public void testProjectDeleteReimport() throws Exception {
@@ -152,54 +135,74 @@ public class JpaModelTests extends TestCase {
 		jpaProject = JptCorePlugin.getJpaProject(project);
 		assertNotNull(jpaProject);
 		assertEquals(4, jpaProject.jpaFilesSize());
-		assertNotNull(jpaProject.getJpaFile(this.file(this.testProject, "src/test/pkg/TestEntity.java")));
-		assertNotNull(jpaProject.getJpaFile(this.file(this.testProject, "src/test/pkg/TestEntity2.java")));
-		assertNotNull(jpaProject.getJpaFile(this.file(this.testProject, "src/META-INF/persistence.xml")));
-		assertNotNull(jpaProject.getJpaFile(this.file(this.testProject, "src/META-INF/orm.xml")));
+		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/test/pkg/TestEntity.java")));
+		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/test/pkg/TestEntity2.java")));
+		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/META-INF/persistence.xml")));
+		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/META-INF/orm.xml")));
+	}
+
+	public void testFacetInstallUninstall() throws Exception {
+		assertNull(JptCorePlugin.getJpaProject(this.testProject.getProject()));
+
+		this.testProject.installFacet("jpt.jpa", "1.0");
+		assertEquals(1, JptCorePlugin.getJpaModel().jpaProjectsSize());
+		JpaProject jpaProject = JptCorePlugin.getJpaProject(this.testProject.getProject());
+		assertNotNull(jpaProject);
+		assertEquals(4, jpaProject.jpaFilesSize());
+		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/test/pkg/TestEntity.java")));
+		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/test/pkg/TestEntity2.java")));
+
+		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/META-INF/persistence.xml")));
+		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/META-INF/orm.xml")));
+
+		this.testProject.uninstallFacet("jpt.jpa", "1.0");
+		assertEquals(0, JptCorePlugin.getJpaModel().jpaProjectsSize());
+		jpaProject = JptCorePlugin.getJpaProject(this.testProject.getProject());
+		assertNull(jpaProject);
 	}
 
 	//TODO - Commented out this test, since it was failing in the I-Build and we're not sure why.
 	//See bug 221757
-//	public void testEditFacetSettingsFile() throws Exception {
-//		assertNull(JptCorePlugin.jpaProject(testProject.getProject()));
-//
-//		// add the JPA facet by modifying the facet settings file directly
-//		IFile facetSettingsFile = this.file(testProject, ".settings/org.eclipse.wst.common.project.facet.core.xml");
-//		InputStream inStream = new BufferedInputStream(facetSettingsFile.getContents());
-//		int fileSize = inStream.available();
-//		byte[] buf = new byte[fileSize];
-//		inStream.read(buf);
-//		inStream.close();
-//
-//		String oldDocument = new String(buf);
-//		String oldString = "<installed facet=\"jst.utility\" version=\"1.0\"/>";
-//		String newString = oldString + CR + "  " + "<installed facet=\"jpt.jpa\" version=\"1.0\"/>";
-//		String newDocument = oldDocument.replaceAll(oldString, newString);
-//
-//		facetSettingsFile.setContents(new ByteArrayInputStream(newDocument.getBytes()), false, false, null);
-//
-//		assertEquals(1, JptCorePlugin.jpaModel().jpaProjectsSize());
-//		JpaProject jpaProject = JptCorePlugin.jpaProject(testProject.getProject());
-//		assertNotNull(jpaProject);
-//		// persistence.xml and orm.xml do not get created in this situation (?)
-//		assertEquals(2, jpaProject.jpaFilesSize());
-//		assertNotNull(jpaProject.jpaFile(this.file(testProject, "src/test.pkg/TestEntity.java")));
-//		assertNotNull(jpaProject.jpaFile(this.file(testProject, "src/test.pkg/TestEntity2.java")));
-//		// assertNotNull(jpaProject.jpaFile(this.file(testProject, "src/META-INF/persistence.xml")));
-//		// assertNotNull(jpaProject.jpaFile(this.file(testProject, "src/META-INF/orm.xml")));
-//
-//		// now remove the JPA facet
-//		facetSettingsFile.setContents(new ByteArrayInputStream(oldDocument.getBytes()), false, false, null);
-//// TODO moved this stuff to the error console until we can figure out why it fails intermittently  ~bjv
-////		assertEquals(0, JptCorePlugin.jpaModel().jpaProjectsSize());
-////		jpaProject = JptCorePlugin.jpaProject(testProject.getProject());
-////		assertNull(jpaProject);
-//		int size = JptCorePlugin.jpaModel().jpaProjectsSize();
-//		if (size != 0) {
-//			System.err.println("bogus size: " + size);
-//			System.err.println("bogus project: " + JptCorePlugin.jpaProject(testProject.getProject()));
-//		}
-//	}
+	public void testEditFacetSettingsFile() throws Exception {
+		assertNull(JptCorePlugin.getJpaProject(this.testProject.getProject()));
+
+		// add the JPA facet by modifying the facet settings file directly
+		IFile facetSettingsFile = this.getFile(this.testProject, ".settings/org.eclipse.wst.common.project.facet.core.xml");
+		InputStream inStream = new BufferedInputStream(facetSettingsFile.getContents());
+		int fileSize = inStream.available();
+		byte[] buf = new byte[fileSize];
+		inStream.read(buf);
+		inStream.close();
+
+		String oldDocument = new String(buf);
+		String oldString = "<installed facet=\"jst.utility\" version=\"1.0\"/>";
+		String newString = oldString + CR + "  " + "<installed facet=\"jpt.jpa\" version=\"1.0\"/>";
+		String newDocument = oldDocument.replaceAll(oldString, newString);
+
+		facetSettingsFile.setContents(new ByteArrayInputStream(newDocument.getBytes()), false, false, null);
+
+		assertEquals(1, JptCorePlugin.getJpaModel().jpaProjectsSize());
+		JpaProject jpaProject = JptCorePlugin.getJpaProject(this.testProject.getProject());
+		assertNotNull(jpaProject);
+		// persistence.xml and orm.xml do not get created in this situation (?)
+		assertEquals(2, jpaProject.jpaFilesSize());
+		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/test/pkg/TestEntity.java")));
+		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/test/pkg/TestEntity2.java")));
+//		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/META-INF/persistence.xml")));
+//		assertNotNull(jpaProject.getJpaFile(this.getFile(this.testProject, "src/META-INF/orm.xml")));
+
+		// now remove the JPA facet
+		facetSettingsFile.setContents(new ByteArrayInputStream(oldDocument.getBytes()), false, false, null);
+// TODO moved this stuff to the error console until we can figure out why it fails intermittently  ~bjv
+//		assertEquals(0, JptCorePlugin.jpaModel().jpaProjectsSize());
+//		jpaProject = JptCorePlugin.jpaProject(testProject.getProject());
+//		assertNull(jpaProject);
+		int size = JptCorePlugin.getJpaModel().jpaProjectsSize();
+		if (size != 0) {
+			System.err.println("bogus size: " + size);
+			System.err.println("bogus project: " + JptCorePlugin.getJpaProject(this.testProject.getProject()));
+		}
+	}
 
 	/**
 	 * make sure the DEBUG constants are 'false' before checking in the code
