b030d45c9277a243db7c3a8fd886df89ac51d8d0
pfullbright <pfullbright>
2008-11-11 15:22:18 +0000

    added provisional api comment

--- jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/resource/JpaResourceModelProviderEvent.java
+++ jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/resource/JpaResourceModelProviderEvent.java
@@ -10,6 +10,13 @@
  *******************************************************************************/
 package org.eclipse.jpt.core.resource;
 
+/**
+ * Provisional API: This interface is part of an interim API that is still
+ * under development and expected to change significantly before reaching
+ * stability. It is available at this early stage to solicit feedback from
+ * pioneering adopters on the understanding that any code that uses this API
+ * will almost certainly be broken (repeatedly) as the API evolves.
+ */
 public class JpaResourceModelProviderEvent
 {
 	public final static int RESOURCE_LOADED = 0x1;
