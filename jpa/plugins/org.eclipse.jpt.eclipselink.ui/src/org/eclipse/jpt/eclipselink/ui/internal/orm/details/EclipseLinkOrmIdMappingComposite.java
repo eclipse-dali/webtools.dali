3a6401c9e5a41799d70d0bac1bb0978bb47bf854
kmoore <kmoore>
2008-11-11 15:50:51 +0000

    one more tweak for converters

--- jpa/plugins/org.eclipse.jpt.eclipselink.ui/src/org/eclipse/jpt/eclipselink/ui/internal/orm/details/EclipseLinkOrmIdMappingComposite.java
+++ jpa/plugins/org.eclipse.jpt.eclipselink.ui/src/org/eclipse/jpt/eclipselink/ui/internal/orm/details/EclipseLinkOrmIdMappingComposite.java
@@ -9,19 +9,13 @@
  ******************************************************************************/
 package org.eclipse.jpt.eclipselink.ui.internal.orm.details;
 
-import org.eclipse.jpt.core.context.Converter;
 import org.eclipse.jpt.core.context.IdMapping;
 import org.eclipse.jpt.eclipselink.core.context.Convert;
 import org.eclipse.jpt.eclipselink.ui.internal.mappings.details.EclipseLinkIdMappingComposite;
 import org.eclipse.jpt.ui.WidgetFactory;
 import org.eclipse.jpt.ui.details.JpaComposite;
-import org.eclipse.jpt.ui.internal.mappings.JptUiMappingsMessages;
-import org.eclipse.jpt.ui.internal.mappings.details.TemporalTypeComposite;
 import org.eclipse.jpt.ui.internal.widgets.Pane;
 import org.eclipse.jpt.utility.model.value.PropertyValueModel;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 
 //Temporary to remove the Converters section from orm basic mapping.
@@ -40,33 +34,6 @@ public class EclipseLinkOrmIdMappingComposite extends EclipseLinkIdMappingCompos
 	                               WidgetFactory widgetFactory) {
 
 		super(subjectHolder, parent, widgetFactory);
-	}
-
-	@Override
-	protected void initializeTypePane(Composite container) {
-
-		container = addCollapsableSection(
-			addSubPane(container, 5),
-			JptUiMappingsMessages.TypeSection_type
-		);
-		((GridLayout) container.getLayout()).numColumns = 2;
-
-		// No converter
-		Button noConverterButton = addRadioButton(
-			container, 
-			JptUiMappingsMessages.TypeSection_default, 
-			buildNoConverterHolder(), 
-			null);
-		((GridData) noConverterButton.getLayoutData()).horizontalSpan = 2;
-				
-		PropertyValueModel<Converter> specifiedConverterHolder = buildSpecifiedConverterHolder();
-		// Temporal
-		addRadioButton(
-			container, 
-			JptUiMappingsMessages.TypeSection_temporal, 
-			buildTemporalBooleanHolder(), 
-			null);
-		registerSubPane(new TemporalTypeComposite(buildTemporalConverterHolder(specifiedConverterHolder), container, getWidgetFactory()));
 	}
 
 	@Override
