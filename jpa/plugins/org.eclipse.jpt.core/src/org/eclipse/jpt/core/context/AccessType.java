f2b056b8029d8ea7faa22b6daddf516aca5d966c
kmoore <kmoore>
2009-01-13 19:18:27 +0000

    added toJavaResourceModel static method

--- jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/context/AccessType.java
+++ jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/context/AccessType.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 Oracle. All rights reserved.
+ * Copyright (c) 2007, 2009 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -35,6 +35,20 @@ public enum AccessType {
 				return PROPERTY;
 			default:
 				throw new IllegalArgumentException("unknown access type: " + javaAccessType); //$NON-NLS-1$
+		}
+	}
+
+	public static org.eclipse.jpt.core.resource.java.AccessType toJavaResourceModel(AccessType accessType) {
+		if (accessType == null) {
+			return null;
+		}
+		switch (accessType) {
+			case FIELD:
+				return org.eclipse.jpt.core.resource.java.AccessType.FIELD;
+			case PROPERTY:
+				return org.eclipse.jpt.core.resource.java.AccessType.PROPERTY;
+			default:
+				throw new IllegalArgumentException("unknown access type: " + accessType); //$NON-NLS-1$
 		}
 	}
 	
