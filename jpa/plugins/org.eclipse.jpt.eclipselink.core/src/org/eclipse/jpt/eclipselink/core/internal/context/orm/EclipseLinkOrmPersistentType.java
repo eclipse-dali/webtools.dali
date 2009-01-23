f3b21e981bb4f7c295683cf0314e0160ecf1e4e6
kmoore <kmoore>
2009-01-23 22:22:28 +0000

    refactored to create AbstractOrmPersistentType

--- jpa/plugins/org.eclipse.jpt.eclipselink.core/src/org/eclipse/jpt/eclipselink/core/internal/context/orm/EclipseLinkOrmPersistentType.java
+++ jpa/plugins/org.eclipse.jpt.eclipselink.core/src/org/eclipse/jpt/eclipselink/core/internal/context/orm/EclipseLinkOrmPersistentType.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 Oracle. All rights reserved.
+ * Copyright (c) 2008, 2009 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -9,11 +9,11 @@
  ******************************************************************************/
 package org.eclipse.jpt.eclipselink.core.internal.context.orm;
 
-import org.eclipse.jpt.core.internal.context.orm.GenericOrmPersistentType;
+import org.eclipse.jpt.core.internal.context.orm.AbstractOrmPersistentType;
 import org.eclipse.jpt.core.resource.orm.Attributes;
 import org.eclipse.jpt.eclipselink.core.resource.orm.EclipseLinkOrmFactory;
 
-public class EclipseLinkOrmPersistentType extends GenericOrmPersistentType
+public class EclipseLinkOrmPersistentType extends AbstractOrmPersistentType
 {
 	
 	public EclipseLinkOrmPersistentType(EclipseLinkEntityMappings parent, String mappingKey) {
