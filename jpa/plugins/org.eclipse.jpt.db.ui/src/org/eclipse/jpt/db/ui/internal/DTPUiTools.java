804761d1d78a98589395e0927dc5278b5dabd00f
tle <tle>
2009-01-31 01:46:03 +0000

    Reverted  211489 - Internal API usage DTPUiTools

--- jpa/plugins/org.eclipse.jpt.db.ui/src/org/eclipse/jpt/db/ui/internal/DTPUiTools.java
+++ jpa/plugins/org.eclipse.jpt.db.ui/src/org/eclipse/jpt/db/ui/internal/DTPUiTools.java
@@ -9,10 +9,18 @@
 *******************************************************************************/
 package org.eclipse.jpt.db.ui.internal;
 
+import org.eclipse.datatools.connectivity.ICategory;
 import org.eclipse.datatools.connectivity.IConnectionProfile;
 import org.eclipse.datatools.connectivity.IProfileListener;
 import org.eclipse.datatools.connectivity.ProfileManager;
-import org.eclipse.datatools.connectivity.db.generic.ui.wizard.NewJDBCFilteredCPWizard;
+import org.eclipse.datatools.connectivity.internal.ConnectionProfileManager;
+import org.eclipse.datatools.connectivity.internal.ui.wizards.CPWizardNode;
+import org.eclipse.datatools.connectivity.internal.ui.wizards.NewCPWizard;
+import org.eclipse.datatools.connectivity.internal.ui.wizards.ProfileWizardProvider;
+import org.eclipse.datatools.connectivity.ui.wizards.IProfileWizardProvider;
+import org.eclipse.datatools.connectivity.ui.wizards.IWizardCategoryProvider;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Display;
@@ -21,6 +29,7 @@ import org.eclipse.swt.widgets.Display;
  * DTP UI tools
  */
 public class DTPUiTools {
+
 
 	/**
 	 * Launch the DTP New Connection Profile wizard to create a new database connection profile.
@@ -31,7 +40,7 @@ public class DTPUiTools {
 	 */
 	public static String createNewConnectionProfile() {
 		// Filter datasource category
-		NewJDBCFilteredCPWizard  wizard = new NewJDBCFilteredCPWizard();
+		NewCPWizard wizard = new NewCPWizard(new LocalViewerFilter(), null);
 		WizardDialog wizardDialog = new WizardDialog(Display.getCurrent().getActiveShell(), wizard);
 		wizardDialog.setBlockOnOpen(true);
 
@@ -72,35 +81,35 @@ public class DTPUiTools {
 	}
 
 
-//	// ********** viewer filter **********
-//
-//	static class LocalViewerFilter extends ViewerFilter {
-//
-//		private static final String DATABASE_CATEGORY_ID = "org.eclipse.datatools.connectivity.db.category"; //$NON-NLS-1$
-//
-//		LocalViewerFilter() {
-//			super();
-//		}
-//
-//		@Override
-//		public boolean select(Viewer viewer, Object parentElement, Object element) {
-//			CPWizardNode wizardNode = (CPWizardNode) element;
-//			IProfileWizardProvider wizardProvider = wizardNode.getProvider();
-//			if (wizardProvider instanceof IWizardCategoryProvider) {
-//				return false;
-//			}
-//			ICategory category = ConnectionProfileManager.getInstance().getProvider(
-//							((ProfileWizardProvider) wizardProvider).getProfile()).getCategory();
-//			
-//			// Only display wizards belong to database category
-//			while (category != null) {
-//				if (category.getId().equals(DATABASE_CATEGORY_ID)) {
-//					return true;
-//				}
-//				category = category.getParent();
-//			}
-//			return false;
-//		}
-//	}
+	// ********** viewer filter **********
+
+	static class LocalViewerFilter extends ViewerFilter {
+
+		private static final String DATABASE_CATEGORY_ID = "org.eclipse.datatools.connectivity.db.category"; //$NON-NLS-1$
+
+		LocalViewerFilter() {
+			super();
+		}
+
+		@Override
+		public boolean select(Viewer viewer, Object parentElement, Object element) {
+			CPWizardNode wizardNode = (CPWizardNode) element;
+			IProfileWizardProvider wizardProvider = wizardNode.getProvider();
+			if (wizardProvider instanceof IWizardCategoryProvider) {
+				return false;
+			}
+			ICategory category = ConnectionProfileManager.getInstance().getProvider(
+							((ProfileWizardProvider) wizardProvider).getProfile()).getCategory();
+			
+			// Only display wizards belong to database category
+			while (category != null) {
+				if (category.getId().equals(DATABASE_CATEGORY_ID)) {
+					return true;
+				}
+				category = category.getParent();
+			}
+			return false;
+		}
+	}
 
 }
