300d2392f4685f1e0d0101e5d2a7d928324165b5
kmoore <kmoore>
2009-04-17 21:07:14 +0000

    230396 - changing spinners to combos with a default option that only accept integers

--- jpa/plugins/org.eclipse.jpt.eclipselink.ui/src/org/eclipse/jpt/eclipselink/ui/internal/mappings/details/CacheSizeComposite.java
+++ jpa/plugins/org.eclipse.jpt.eclipselink.ui/src/org/eclipse/jpt/eclipselink/ui/internal/mappings/details/CacheSizeComposite.java
@@ -10,20 +10,12 @@ package org.eclipse.jpt.eclipselink.ui.internal.mappings.details;
 
 import org.eclipse.jpt.eclipselink.core.context.Caching;
 import org.eclipse.jpt.eclipselink.ui.internal.mappings.EclipseLinkUiMappingsMessages;
-import org.eclipse.jpt.ui.internal.mappings.JptUiMappingsMessages;
-import org.eclipse.jpt.ui.internal.util.LabeledControlUpdater;
-import org.eclipse.jpt.ui.internal.util.LabeledLabel;
 import org.eclipse.jpt.ui.internal.widgets.FormPane;
+import org.eclipse.jpt.ui.internal.widgets.IntegerCombo;
 import org.eclipse.jpt.utility.internal.model.value.PropertyAspectAdapter;
-import org.eclipse.jpt.utility.internal.model.value.TransformationPropertyValueModel;
 import org.eclipse.jpt.utility.model.value.PropertyValueModel;
 import org.eclipse.jpt.utility.model.value.WritablePropertyValueModel;
-import org.eclipse.osgi.util.NLS;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Spinner;
 
 /**
  * Here is the layout of this pane:
@@ -56,121 +48,47 @@ public class CacheSizeComposite extends FormPane<Caching> {
 
 	@Override
 	protected void initializeLayout(Composite container) {
-
-		// Size widgets
-		Spinner sizeSpinner = addLabeledSpinner(
-			container,
-			EclipseLinkUiMappingsMessages.CacheSizeComposite_size,
-			buildSizeHolder(),
-			-1,
-			-1,
-			Integer.MAX_VALUE,
-			addDefaultSizeLabel(container),
-			null//TODO JpaHelpContextIds.MAPPING_COLUMN_LENGTH
-		);
-
-		updateGridData(container, sizeSpinner);
+		addSizeCombo(container);
 	}
 	
-	private WritablePropertyValueModel<Integer> buildSizeHolder() {
-		return new PropertyAspectAdapter<Caching, Integer>(getSubjectHolder(), Caching.SPECIFIED_SIZE_PROPERTY) {
+	private void addSizeCombo(Composite container) {
+		new IntegerCombo<Caching>(this, container) {
+			
 			@Override
-			protected Integer buildValue_() {
-				return subject.getSpecifiedSize();
+			protected String getLabelText() {
+				return EclipseLinkUiMappingsMessages.CacheSizeComposite_size;
+			}
+		
+			@Override
+			protected String getHelpId() {
+				return null;//JpaHelpContextIds.MAPPING_COLUMN_LENGTH;
 			}
 
 			@Override
-			protected void setValue_(Integer value) {
-				if (value == -1) {
-					value = null;
-				}
-				subject.setSpecifiedSize(value);
+			protected PropertyValueModel<Integer> buildDefaultHolder() {
+				return new PropertyAspectAdapter<Caching, Integer>(getSubjectHolder(), Caching.DEFAULT_SIZE_PROPERTY) {
+					@Override
+					protected Integer buildValue_() {
+						return Integer.valueOf(this.subject.getDefaultSize());
+					}
+				};
 			}
-		};
-	}
-
-	private WritablePropertyValueModel<Integer> buildDefaultSizeHolder() {
-		return new PropertyAspectAdapter<Caching, Integer>(getSubjectHolder(), Caching.DEFAULT_SIZE_PROPERTY) {
+			
 			@Override
-			protected Integer buildValue_() {
-				return subject.getDefaultSize();
-			}
+			protected WritablePropertyValueModel<Integer> buildSelectedItemHolder() {
+				return new PropertyAspectAdapter<Caching, Integer>(getSubjectHolder(), Caching.SPECIFIED_SIZE_PROPERTY) {
+					@Override
+					protected Integer buildValue_() {
+						return this.subject.getSpecifiedSize();
+					}
 
-			@Override
-			protected void subjectChanged() {
-				Object oldValue = this.getValue();
-				super.subjectChanged();
-				Object newValue = this.getValue();
-
-				// Make sure the default value is appended to the text
-				if (oldValue == newValue && newValue == null) {
-					this.fireAspectChange(Integer.MIN_VALUE, newValue);
-				}
-			}
-		};
-	}
-
-	private Control addDefaultSizeLabel(Composite container) {
-
-		Label label = addLabel(
-			container,
-			JptUiMappingsMessages.DefaultEmpty
-		);
-
-		new LabeledControlUpdater(
-			new LabeledLabel(label),
-			buildDefaultSizeLabelHolder()
-		);
-
-		return label;
-	}
-
-	private PropertyValueModel<String> buildDefaultSizeLabelHolder() {
-
-		return new TransformationPropertyValueModel<Integer, String>(buildDefaultSizeHolder()) {
-
-			@Override
-			protected String transform(Integer value) {
-
-				Integer defaultValue = (getSubject() != null) ? getSubject().getDefaultSize() :
-								Caching.DEFAULT_SIZE;
-
-				return NLS.bind(
-					JptUiMappingsMessages.DefaultWithOneParam,
-					defaultValue
-				);
+					@Override
+					protected void setValue_(Integer value) {
+						this.subject.setSpecifiedSize(value);
+					}
+				};
 			}
 		};
 	}
 
-	/**
-	 * Changes the layout of the given container by changing which widget will
-	 * grab the excess of horizontal space. By default, the center control grabs
-	 * the excess space, we change it to be the right control.
-	 *
-	 * @param container The container containing the controls needing their
-	 * <code>GridData</code> to be modified from the default values
-	 * @param spinner The spinner that got created
-	 */
-	private void updateGridData(Composite container, Spinner spinner) {
-
-		// It is possible the spinner's parent is not the container of the
-		// label, spinner and right control (a pane is sometimes required for
-		// painting the spinner's border)
-		Composite paneContainer = spinner.getParent();
-
-		while (container != paneContainer.getParent()) {
-			paneContainer = paneContainer.getParent();
-		}
-
-		Control[] controls = paneContainer.getChildren();
-
-		GridData gridData = new GridData();
-		gridData.grabExcessHorizontalSpace = false;
-		gridData.horizontalAlignment       = GridData.BEGINNING;
-		controls[1].setLayoutData(gridData);
-
-		controls[2].setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-		removeAlignRight(controls[2]);
-	}
 }
