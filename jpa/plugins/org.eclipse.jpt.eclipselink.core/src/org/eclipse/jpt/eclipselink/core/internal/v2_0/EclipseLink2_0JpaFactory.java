f7c2a24ab2954b2b8a96099546a6ad9a51c8a7e6
tle <tle>
2009-08-18 16:05:54 +0000

    Preliminary EclipseLink 2.0 platform

---
+++ jpa/plugins/org.eclipse.jpt.eclipselink.core/src/org/eclipse/jpt/eclipselink/core/internal/v2_0/EclipseLink2_0JpaFactory.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+* Copyright (c) 2009 Oracle. All rights reserved.
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v1.0, which accompanies this distribution
+* and is available at http://www.eclipse.org/legal/epl-v10.html.
+* 
+* Contributors:
+*     Oracle - initial API and implementation
+*******************************************************************************/
+package org.eclipse.jpt.eclipselink.core.internal.v2_0;
+
+import org.eclipse.jpt.eclipselink.core.internal.v1_1.EclipseLink1_1JpaFactory;
+
+/**
+ *  EclipseLink2_0JpaFactory
+ */
+public class EclipseLink2_0JpaFactory
+	extends EclipseLink1_1JpaFactory
+{
+	protected EclipseLink2_0JpaFactory() {
+		super();
+	}
+	
+	
+}
