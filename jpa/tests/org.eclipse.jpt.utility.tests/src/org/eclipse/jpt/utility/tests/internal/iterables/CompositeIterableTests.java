7736eaba25a1acb559f9e128c25e1ccf49c7f568
bvosburgh <bvosburgh>
2009-11-07 05:52:50 +0000

    add some tests

--- jpa/tests/org.eclipse.jpt.utility.tests/src/org/eclipse/jpt/utility/tests/internal/iterables/CompositeIterableTests.java
+++ jpa/tests/org.eclipse.jpt.utility.tests/src/org/eclipse/jpt/utility/tests/internal/iterables/CompositeIterableTests.java
@@ -44,4 +44,72 @@ public class CompositeIterableTests extends TestCase {
 		}
 	}
 
+	public void testExtraElement1() {
+		Collection<String> c1 = new ArrayList<String>();
+		c1.add("0");
+		c1.add("1");
+		c1.add("2");
+		c1.add("3");
+
+		Iterable<String> composite = new CompositeIterable<String>(c1, "4");
+		int i = 0;
+		for (String s : composite) {
+			assertEquals(String.valueOf(i++), s);
+		}
+	}
+
+	public void testExtraElement2() {
+		Collection<String> c1 = new ArrayList<String>();
+		c1.add("1");
+		c1.add("2");
+		c1.add("3");
+
+		Iterable<String> composite = new CompositeIterable<String>("0", c1);
+		int i = 0;
+		for (String s : composite) {
+			assertEquals(String.valueOf(i++), s);
+		}
+	}
+
+	public void testCollectionOfIterables() {
+		Collection<String> c1 = new ArrayList<String>();
+		c1.add("0");
+		c1.add("1");
+		c1.add("2");
+		c1.add("3");
+
+		Collection<String> c2 = new ArrayList<String>();
+		c2.add("4");
+		c2.add("5");
+		c2.add("6");
+		c2.add("7");
+
+		Collection<Iterable<String>> collection = new ArrayList<Iterable<String>>();
+		collection.add(c1);
+		collection.add(c2);
+		Iterable<String> composite = new CompositeIterable<String>(collection);
+		int i = 0;
+		for (String s : composite) {
+			assertEquals(String.valueOf(i++), s);
+		}
+	}
+
+	public void testToString() {
+		Collection<String> c1 = new ArrayList<String>();
+		c1.add("0");
+		c1.add("1");
+		c1.add("2");
+		c1.add("3");
+
+		Collection<String> c2 = new ArrayList<String>();
+		c2.add("4");
+		c2.add("5");
+		c2.add("6");
+		c2.add("7");
+
+		@SuppressWarnings("unchecked")
+		Iterable<String> composite = new CompositeIterable<String>(c1, c2);
+		assertNotNull(composite.toString());
+	}
+
 }
