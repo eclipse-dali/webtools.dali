5c3cc4a22df3c7bbbed0ec72e383557a3277b571
tle <tle>
2009-12-01 02:09:41 +0000

    Added comment.

--- jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/jpa2/context/SequenceGenerator2_0.java
+++ jpa/plugins/org.eclipse.jpt.core/src/org/eclipse/jpt/core/jpa2/context/SequenceGenerator2_0.java
@@ -12,7 +12,14 @@ package org.eclipse.jpt.core.jpa2.context;
 import org.eclipse.jpt.core.context.SequenceGenerator;
 
 /**
- *  SequenceGenerator2_0
+ * Provisional API: This interface is part of an interim API that is still
+ * under development and expected to change significantly before reaching
+ * stability. It is available at this early stage to solicit feedback from
+ * pioneering adopters on the understanding that any code that uses this API
+ * will almost certainly be broken (repeatedly) as the API evolves.
+ * 
+ * @version 3.0
+ * @since 3.0
  */
 public interface SequenceGenerator2_0 extends SequenceGenerator
 {
