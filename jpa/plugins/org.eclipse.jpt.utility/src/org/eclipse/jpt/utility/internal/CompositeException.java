0097473c485ce6efa9dd3f1d20914336b47464f1
bvosburgh <bvosburgh>
2010-03-08 17:12:33 +0000

    add constructor

--- jpa/plugins/org.eclipse.jpt.utility/src/org/eclipse/jpt/utility/internal/CompositeException.java
+++ jpa/plugins/org.eclipse.jpt.utility/src/org/eclipse/jpt/utility/internal/CompositeException.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 Oracle. All rights reserved.
+ * Copyright (c) 2009, 2010 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -9,6 +9,8 @@
  ******************************************************************************/
 package org.eclipse.jpt.utility.internal;
 
+import java.util.Collection;
+
 /**
  * Provide a way for multiple exceptions to be packaged and reported.
  */
@@ -17,6 +19,14 @@ public class CompositeException
 {
 	private final Throwable[] exceptions;
 	private static final long serialVersionUID = 1L;
+
+
+	/**
+	 * The specified exceptions list must not be empty.
+	 */
+	public CompositeException(Collection<Throwable> exceptions) {
+		this(exceptions.toArray(new Throwable[exceptions.size()]));
+	}
 
 	/**
 	 * The specified exceptions list must not be empty.
