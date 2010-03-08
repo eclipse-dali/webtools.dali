95b1019ade899321e44dfa868efb0d06e087dd43
bvosburgh <bvosburgh>
2010-03-08 17:12:45 +0000

    tweak comments

--- jpa/plugins/org.eclipse.jpt.utility/src/org/eclipse/jpt/utility/internal/AbstractAssociation.java
+++ jpa/plugins/org.eclipse.jpt.utility/src/org/eclipse/jpt/utility/internal/AbstractAssociation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2009 Oracle. All rights reserved.
+ * Copyright (c) 2008, 2010 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -10,7 +10,7 @@
 package org.eclipse.jpt.utility.internal;
 
 /**
- * Implement some of the methods in Association that can
+ * Implement some of the methods in {@link Association} that can
  * be defined in terms of the other methods.
  */
 public abstract class AbstractAssociation<K, V>
--- jpa/plugins/org.eclipse.jpt.utility/src/org/eclipse/jpt/utility/internal/SimpleAssociation.java
+++ jpa/plugins/org.eclipse.jpt.utility/src/org/eclipse/jpt/utility/internal/SimpleAssociation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 Oracle. All rights reserved.
+ * Copyright (c) 2008, 2010 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -12,7 +12,7 @@ package org.eclipse.jpt.utility.internal;
 import java.io.Serializable;
 
 /**
- * Straightforward implementation of Association.
+ * Straightforward implementation of {@ link Association}.
  */
 public class SimpleAssociation<K, V>
 	extends AbstractAssociation<K, V>
