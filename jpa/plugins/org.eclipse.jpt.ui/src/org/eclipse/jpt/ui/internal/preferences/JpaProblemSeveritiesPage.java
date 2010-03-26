ff6d0e48df2d2502b31c1e2964426433fd293b8e
bvosburgh <bvosburgh>
2010-03-26 14:16:46 +0000

    try to improve resizing behavior

--- jpa/plugins/org.eclipse.jpt.ui/src/org/eclipse/jpt/ui/internal/preferences/JpaProblemSeveritiesPage.java
+++ jpa/plugins/org.eclipse.jpt.ui/src/org/eclipse/jpt/ui/internal/preferences/JpaProblemSeveritiesPage.java
@@ -40,6 +40,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Combo;
@@ -201,6 +202,11 @@ public class JpaProblemSeveritiesPage extends PropertyAndPreferencePage {
 		restoreSectionExpansionStates(dialogPreferences());
 
 		return container;
+	}
+
+	@Override
+	public Point computeSize() {
+		return this.doComputeSize();
 	}
 
 	private void addProjectLevelCategory(Composite parent) {
