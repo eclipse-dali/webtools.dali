8f5a52b44f9e23ae72d02a59302d2fe43a6d0f86
kmoore <kmoore>
2010-10-18 20:54:51 +0000

    added attribute level annotation Strings

--- jaxb/plugins/org.eclipse.jpt.jaxb.core/src/org/eclipse/jpt/jaxb/core/resource/java/JAXB.java
+++ jaxb/plugins/org.eclipse.jpt.jaxb.core/src/org/eclipse/jpt/jaxb/core/resource/java/JAXB.java
@@ -27,31 +27,80 @@ public interface JAXB {
 	// JAXB package
 	String PACKAGE = "javax.xml.bind.annotation";
 	String PACKAGE_ = PACKAGE + '.';
-	
+
 	String ADAPTERS_PACKAGE = PACKAGE_ + "adapters";
-	String ADAPTERS_PACKAGE_ = ADAPTERS_PACKAGE + ".";
-	
+	String ADAPTERS_PACKAGE_ = ADAPTERS_PACKAGE + '.';
 
 	// ********** API **********
 
 	// JAXB annotations
+	String XML_ATTACHMENT_REF = PACKAGE_ + "XmlAttachmentRef";
+
 	String XML_ACCESSOR_ORDER = PACKAGE_ + "XmlAccessorOrder";
 		String XML_ACCESSOR_ORDER__VALUE = "value";
 
 	String XML_ACCESSOR_TYPE = PACKAGE_ + "XmlAccessorType";
 		String XML_ACCESSOR_TYPE__VALUE = "value";
 
+	String XML_ANY_ATTRIBUTE = PACKAGE_ + "XmlAnyAttribute";
+
+	String XML_ANY_ELEMENT = PACKAGE_ + "XmlAnyElement";
+		String XML_ANY_ELEMENT__LAX = "lax";
+		String XML_ANY_ELEMENT__VALUE = "value";
+
+	String XML_ATTRIBUTE = PACKAGE_ + "XmlAttribute";
+		String XML_ATTRIBUTE__NAME = "name";
+		String XML_ATTRIBUTE__NAMESPACE = "namespace";
+		String XML_ATTRIBUTE__REQUIRED = "required";
+
+	String XML_ELEMENT = PACKAGE_ + "XmlElement";
+		String XML_ELEMENT__NAME = "name";
+		String XML_ELEMENT__NAMESPACE = "namespace";
+		String XML_ELEMENT__DEFAULT_VALUE = "defaultValue";
+		String XML_ELEMENT__NILLABLE = "nillable";
+		String XML_ELEMENT__REQUIRED = "required";
+		String XML_ELEMENT__TYPE = "type";
+
+	String XML_ELEMENTS = PACKAGE_ + "XmlElements";
+		String XML_ELEMENTS__VALUE = "value";
+
+	String XML_ELEMENT_REF = PACKAGE_ + "XmlElementRef";
+		String XML_ELEMENT_REF__NAME = "name";
+		String XML_ELEMENT_REF__NAMESPACE = "namespace";
+		String XML_ELEMENT_REF__TYPE = "type";
+
+	String XML_ELEMENT_REFS = PACKAGE_ + "XmlElementRefs";
+		String XML_ELEMENT_REFS__VALUE = "value";
+
+	String XML_ELEMENT_WRAPPER = PACKAGE_ + "XmlElementWrapper";
+		String XML_ELEMENT_WRAPPER__NAME = "name";
+		String XML_ELEMENT_WRAPPER__NAMESPACE = "namespace";
+		String XML_ELEMENT_WRAPPER__NILLABLE = "nillable";
+		String XML_ELEMENT_WRAPPER__REQUIRED = "required";
+
 	String XML_ENUM = PACKAGE_ + "XmlEnum";
 		String XML_ENUM__VALUE = "value";
+
+	String XML_ENUM_VALUE = PACKAGE_ + "XmlEnumValue";
 	
 	String XML_INLINE_BINARY_DATA = PACKAGE_ + "XmlInlineBinaryData";
-	
+
+	String XML_ID = PACKAGE_ + "XmlID";
+
+	String XML_IDREF = PACKAGE_ + "XmlIDREF";
+
 	String XML_JAVA_TYPE_ADAPTER = ADAPTERS_PACKAGE_ + "XmlJavaTypeAdapter";
 		String XML_JAVA_TYPE_ADAPTER__TYPE = "type";
 		String XML_JAVA_TYPE_ADAPTER__VALUE = "value";
 	
 	String XML_JAVA_TYPE_ADAPTERS = ADAPTERS_PACKAGE_ + "XmlJavaTypeAdapters";
 		String XML_JAVA_TYPE_ADAPTERS__VALUE = "value";
+
+	String XML_LIST = PACKAGE_ + "XmlList";
+
+	String XML_MIME_TYPE = PACKAGE_ + "XmlMimeType";
+
+	String XML_MIXED = PACKAGE_ + "XmlMixed";
 	
 	String XML_NS = PACKAGE_ + "XmlNs";
 		String XML_NS__NAMESPACE_URI = "namespaceURI";
@@ -90,6 +139,9 @@ public interface JAXB {
 
 	String XML_TRANSIENT = PACKAGE_ + "XmlTransient";
 
+	String XML_VALUE = PACKAGE_ + "XmlValue";
+
+
 	// JAXB enums
 	String XML_ACCESS_ORDER = PACKAGE_ + "XmlAccessOrder";
 		String XML_ACCESS_ORDER_ = XML_ACCESS_ORDER + '.';
@@ -108,4 +160,5 @@ public interface JAXB {
 		String XML_NS_FORM__QUALIFIED = XML_NS_FORM_ + "QUALIFIED";
 		String XML_NS_FORM__UNQUALIFIED = XML_NS_FORM_ + "UNQUALIFIED";
 		String XML_NS_FORM__UNSET = XML_NS_FORM_ + "UNSET";
+
 }
