08b33c99f5eaa65410c785ab5e523bf9b4149dab
pfullbright <pfullbright>
2011-01-11 22:48:40 +0000

    Added basic Xsd adapter model - schema and types

---
+++ jaxb/plugins/org.eclipse.jpt.jaxb.core/src/org/eclipse/jpt/jaxb/core/xsd/XsdSchema.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ *  Copyright (c) 2011  Oracle. All rights reserved.
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Eclipse Public License v1.0, which accompanies this distribution
+ *  and is available at http://www.eclipse.org/legal/epl-v10.html
+ *  
+ *  Contributors: 
+ *  	Oracle - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jpt.jaxb.core.xsd;
+
+import org.eclipse.emf.common.notify.impl.AdapterImpl;
+import org.eclipse.jpt.utility.internal.iterables.TransformationIterable;
+import org.eclipse.xsd.XSDSchema;
+import org.eclipse.xsd.XSDTypeDefinition;
+
+public class XsdSchema
+		extends AdapterImpl {
+	
+	protected final XSDSchema xsdSchema;
+	
+	
+	
+	XsdSchema(XSDSchema xsdSchema) {
+		this.xsdSchema = xsdSchema;
+	}
+	
+	
+	public Iterable<XsdTypeDefinition> getTypeDefinitions() {
+		return new TransformationIterable<XSDTypeDefinition, XsdTypeDefinition>(this.xsdSchema.getTypeDefinitions()) {
+			@Override
+			protected XsdTypeDefinition transform(XSDTypeDefinition o) {
+				return (XsdTypeDefinition) XsdUtil.getAdapter(o);
+			}
+		};
+	}
+	
+	public XsdTypeDefinition getTypeDefinition(String namespace, String name) {
+		for (XSDTypeDefinition typeDefinition : this.xsdSchema.getTypeDefinitions()) {
+			if (typeDefinition.getTargetNamespace().equals(namespace) && typeDefinition.getName().equals(name)) {
+				return (XsdTypeDefinition) XsdUtil.getAdapter(typeDefinition);
+			}
+		}
+		return null;
+	}
+}
