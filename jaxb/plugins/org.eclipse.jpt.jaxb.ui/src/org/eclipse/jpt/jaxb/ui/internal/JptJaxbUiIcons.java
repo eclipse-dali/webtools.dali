1c0806a73320ad33668cfe30b5be1ca551cfcb59
pfullbright <pfullbright>
2011-02-15 18:02:18 +0000

    persistent class, persistent enum, transient class icons

--- jaxb/plugins/org.eclipse.jpt.jaxb.ui/src/org/eclipse/jpt/jaxb/ui/internal/JptJaxbUiIcons.java
+++ jaxb/plugins/org.eclipse.jpt.jaxb.ui/src/org/eclipse/jpt/jaxb/ui/internal/JptJaxbUiIcons.java
@@ -16,8 +16,11 @@ public class JptJaxbUiIcons
 	
 	public static final String JAXB_CONTENT = "full/obj16/jaxb_content";
 	public static final String PACKAGE = "full/obj16/package";
+	
 	public static final String PERSISTENT_CLASS = "full/obj16/persistent_class";
 	public static final String PERSISTENT_ENUM = "full/obj16/persistent_enum";
+	public static final String TRANSIENT_CLASS = "full/obj16/transient_class";
+	
 	public static final String PERSISTENT_FIELD = "full/obj16/persistent_field";
 	public static final String PERSISTENT_PROPERTY = "full/obj16/persistent_property";
 	public static final String ENUM_CONSTANT = "full/obj16/enum_constant";
