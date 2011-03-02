47fd225a4c4e78e632da0d0697adde8322ee8078
kmoore <kmoore>
2011-03-02 14:31:40 +0000

    335697 - default XmlElement attribute mappings

--- jaxb/plugins/org.eclipse.jpt.jaxb.core/src/org/eclipse/jpt/jaxb/core/internal/context/java/JavaXmlElementMappingDefinition.java
+++ jaxb/plugins/org.eclipse.jpt.jaxb.core/src/org/eclipse/jpt/jaxb/core/internal/context/java/JavaXmlElementMappingDefinition.java
@@ -9,12 +9,14 @@
  ******************************************************************************/
 package org.eclipse.jpt.jaxb.core.internal.context.java;
 
+import java.util.Collection;
 import org.eclipse.jpt.common.utility.internal.iterables.ArrayListIterable;
 import org.eclipse.jpt.jaxb.core.JaxbFactory;
 import org.eclipse.jpt.jaxb.core.MappingKeys;
 import org.eclipse.jpt.jaxb.core.context.JaxbAttributeMapping;
 import org.eclipse.jpt.jaxb.core.context.JaxbPersistentAttribute;
 import org.eclipse.jpt.jaxb.core.context.java.DefaultJavaAttributeMappingDefinition;
+import org.eclipse.jpt.jaxb.core.resource.java.JavaResourceAttribute;
 import org.eclipse.jpt.jaxb.core.resource.java.XmlAttachmentRefAnnotation;
 import org.eclipse.jpt.jaxb.core.resource.java.XmlElementAnnotation;
 import org.eclipse.jpt.jaxb.core.resource.java.XmlElementWrapperAnnotation;
@@ -78,8 +80,26 @@ public class JavaXmlElementMappingDefinition
 		return factory.buildJavaXmlElementMapping(parent);
 	}
 
+	/**
+	 * From the JAXB spec section 8.12.5.1 Default Mapping:
+	 * <p>
+	 * A single valued property or field must be mapped by with the following default mapping annotation:<ul>
+	 * <li> @XmlElement
+	 * </ul>
+	 * <p>
+	 * A property or field with a collection type must be mapped by with the following default mapping annotation:<ul>
+	 * <li> if the property or field is annotated with @XmlList, then the default mapping annotation is:<ul>
+	 * <li> @XmlElement
+	 * </ul>
+	 * <li> otherwise the default mapping annotation is:<ul>
+	 * <li> @XmlElements({ @XmlElement(nillable=true)})
+	 * </ul>
+	 */
 	public boolean isDefault(JaxbPersistentAttribute persistentAttribute) {
-		//TODO define what makes a persistent attribute a default XmlElement mapping
-		return false;
+		JavaResourceAttribute resourceAttribute = persistentAttribute.getJavaResourceAttribute();
+		if (resourceAttribute.typeIsSubTypeOf(Collection.class.getName())) {
+			return resourceAttribute.getAnnotation(XmlListAnnotation.ANNOTATION_NAME) != null;
+		}
+		return true;
 	}
 }
