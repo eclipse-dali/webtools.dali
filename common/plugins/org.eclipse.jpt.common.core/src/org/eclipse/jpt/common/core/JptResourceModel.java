322b5137f41dcd827eca3bfc02f985c98b0c02c9
bvosburgh <bvosburgh>
2011-04-28 02:01:03 +0000

    fix comment

--- common/plugins/org.eclipse.jpt.common.core/src/org/eclipse/jpt/common/core/JptResourceModel.java
+++ common/plugins/org.eclipse.jpt.common.core/src/org/eclipse/jpt/common/core/JptResourceModel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2010 Oracle. All rights reserved.
+ * Copyright (c) 2008, 2011 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -37,7 +37,7 @@ public interface JptResourceModel {
 	void addResourceModelListener(JptResourceModelListener listener);
 
 	/**
-	 * @see #addResourceModelListener(ResourceModelListener)
+	 * @see #addResourceModelListener(JptResourceModelListener)
 	 */
 	void removeResourceModelListener(JptResourceModelListener listener);
 
