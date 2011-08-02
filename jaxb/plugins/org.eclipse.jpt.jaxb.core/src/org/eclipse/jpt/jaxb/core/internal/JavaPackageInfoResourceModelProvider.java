8198a67f511f63af5da000414ae95119075e76d5
kmoore <kmoore>
2011-08-02 20:04:26 +0000

    bug 353495 - NPE in JDTPackage constructor for default packages - fix from Dmitry

--- jaxb/plugins/org.eclipse.jpt.jaxb.core/src/org/eclipse/jpt/jaxb/core/internal/JavaPackageInfoResourceModelProvider.java
+++ jaxb/plugins/org.eclipse.jpt.jaxb.core/src/org/eclipse/jpt/jaxb/core/internal/JavaPackageInfoResourceModelProvider.java
@@ -11,10 +11,13 @@ package org.eclipse.jpt.jaxb.core.internal;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.content.IContentType;
+import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jpt.common.core.JptCommonCorePlugin;
 import org.eclipse.jpt.jaxb.core.JaxbProject;
 import org.eclipse.jpt.jaxb.core.JaxbResourceModelProvider;
+import org.eclipse.jpt.jaxb.core.JptJaxbCorePlugin;
 import org.eclipse.jpt.jaxb.core.internal.resource.java.source.SourcePackageInfoCompilationUnit;
 import org.eclipse.jpt.jaxb.core.resource.java.JavaResourceCompilationUnit;
 
@@ -46,12 +49,23 @@ public class JavaPackageInfoResourceModelProvider
 	}
 
 	public JavaResourceCompilationUnit buildResourceModel(JaxbProject jaxbProject, IFile file) {
-		return new SourcePackageInfoCompilationUnit(
-			JavaCore.createCompilationUnitFrom(file),
-			jaxbProject.getPlatform().getAnnotationProvider(),
-			jaxbProject.getPlatform().getAnnotationEditFormatter(),
-			jaxbProject.getModifySharedDocumentCommandExecutor()
-		);
+		ICompilationUnit compilationUnit = JavaCore.createCompilationUnitFrom(file);
+		try {
+			if (compilationUnit.getPackageDeclarations().length > 0){
+				return new SourcePackageInfoCompilationUnit(
+						compilationUnit,
+						jaxbProject.getPlatform().getAnnotationProvider(),
+						jaxbProject.getPlatform().getAnnotationEditFormatter(),
+						jaxbProject.getModifySharedDocumentCommandExecutor());
+			} 
+			//ignore package-info placed in default package as
+			//it doesn't have package declaration and can't hold annotations
+			return null;
+		} catch (JavaModelException e) {
+			JptJaxbCorePlugin.log(e);
+			// Ignore -- project is in a bad state. This will get recalled if necessary
+			return null;
+		}
 	}
 
 }
