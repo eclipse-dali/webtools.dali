946358b1e4fe50e60bb8d89630e3092e22e91993
kmoore <kmoore>
2011-11-08 15:08:05 +0000

    355415 - Create JPA project and Add New JPA Entity and receive Stack Overflow Error

--- jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/jpa1/context/orm/GenericOrmPersistentType.java
+++ jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/jpa1/context/orm/GenericOrmPersistentType.java
@@ -1318,8 +1318,7 @@ public class GenericOrmPersistentType
 	}
 
 	public TextRange getValidationTextRange() {
-		TextRange textRange = this.mapping.getValidationTextRange();
-		return (textRange != null) ? textRange : this.getEntityMappings().getValidationTextRange();
+		return this.mapping.getValidationTextRange();
 	}
 
 
