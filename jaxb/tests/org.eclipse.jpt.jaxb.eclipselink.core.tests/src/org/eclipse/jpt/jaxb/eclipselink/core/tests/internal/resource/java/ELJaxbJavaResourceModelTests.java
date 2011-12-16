0392217eb63725afc789143fea1545780a1d70d2
pfullbright <pfullbright>
2011-12-16 17:28:31 +0000

    XmlPath annotation tests

--- jaxb/tests/org.eclipse.jpt.jaxb.eclipselink.core.tests/src/org/eclipse/jpt/jaxb/eclipselink/core/tests/internal/resource/java/ELJaxbJavaResourceModelTests.java
+++ jaxb/tests/org.eclipse.jpt.jaxb.eclipselink.core.tests/src/org/eclipse/jpt/jaxb/eclipselink/core/tests/internal/resource/java/ELJaxbJavaResourceModelTests.java
@@ -20,6 +20,7 @@ public class ELJaxbJavaResourceModelTests
 	public static Test suite() {
 		TestSuite suite = new TestSuite(ELJaxbJavaResourceModelTests.class.getName());
 		suite.addTestSuite(XmlInverseReferenceAnnotationTests.class);
+		suite.addTestSuite(XmlPathAnnotationTests.class);
 		suite.addTestSuite(XmlTransformationAnnotationTests.class);
 		
 		return suite;
