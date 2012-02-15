8db91ff9ec6c3650dbddf667d7a99c6531ee49f4
kmoore <kmoore>
2012-02-15 00:14:37 +0000

    we have fully qualified class name API now, let's use it here

--- jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/jpa2/context/orm/OrmMapKeyTemporalConverterValidator.java
+++ jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/jpa2/context/orm/OrmMapKeyTemporalConverterValidator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Oracle. All rights reserved.
+ * Copyright (c) 2011, 2012 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -29,9 +29,7 @@ public class OrmMapKeyTemporalConverterValidator extends AbstractTemporalConvert
 
 	@Override
 	protected String getTypeName() {
-		//no need to worry about the orm.xml package element because we are
-		//only looking for java.util classes, we don't need to append the package element
-		return this.getAttributeMapping().getMapKeyClass();
+		return this.getAttributeMapping().getFullyQualifiedMapKeyClass();
 	}
 
 	@Override
--- jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/jpa2/context/orm/OrmTemporalConverterValidator.java
+++ jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/jpa2/context/orm/OrmTemporalConverterValidator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Oracle. All rights reserved.
+ * Copyright (c) 2011, 2012 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -23,8 +23,6 @@ public class OrmTemporalConverterValidator extends AbstractTemporalConverterVali
 
 	@Override
 	protected String getTypeName() {
-		//no need to worry about the orm.xml package element because we are
-		//only looking for java.util classes, we don't need to append the package element
 		return this.getPersistentAttribute().getTypeName();
 	}
 
