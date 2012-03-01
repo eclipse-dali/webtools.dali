40955a2ccd756fcf4e5be54e0c61007d74e8fe12
bvosburgh <bvosburgh>
2012-03-01 00:00:36 +0000

    add toString(...)

--- common/plugins/org.eclipse.jpt.common.core/src/org/eclipse/jpt/common/core/internal/resource/java/source/SourceNamedAnnotation.java
+++ common/plugins/org.eclipse.jpt.common.core/src/org/eclipse/jpt/common/core/internal/resource/java/source/SourceNamedAnnotation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Oracle. All rights reserved.
+ * Copyright (c) 2011, 2012 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -38,5 +38,10 @@ public final class SourceNamedAnnotation
 	
 	public void synchronizeWith(CompilationUnit astRoot) {
 		//nothing
+	}
+
+	@Override
+	public void toString(StringBuilder sb) {
+		sb.append(this.annotationName);
 	}
 }
