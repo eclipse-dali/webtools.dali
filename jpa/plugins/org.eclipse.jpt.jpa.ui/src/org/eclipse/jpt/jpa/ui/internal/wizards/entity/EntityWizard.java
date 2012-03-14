d4912758e6e45b7d8baaf63c6a4bcf993a4f1b22
bvosburgh <bvosburgh>
2012-03-14 20:45:07 +0000

    fix NPE when nothing selected when wizard opened

--- jpa/plugins/org.eclipse.jpt.jpa.ui/src/org/eclipse/jpt/jpa/ui/internal/wizards/entity/EntityWizard.java
+++ jpa/plugins/org.eclipse.jpt.jpa.ui/src/org/eclipse/jpt/jpa/ui/internal/wizards/entity/EntityWizard.java
@@ -1,14 +1,13 @@
-/***********************************************************************
- * Copyright (c) 2008, 2012 by SAP AG, Walldorf and others. 
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
+/*******************************************************************************
+ * Copyright (c) 2008, 2012 SAP AG, Walldorf and others. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0, which accompanies this distribution
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ * 
  * Contributors:
  *     SAP AG - initial API and implementation
  *     Dimiter Dimitrov, d.dimitrov@sap.com - initial API and implementation     
- ***********************************************************************/
+ ******************************************************************************/
 package org.eclipse.jpt.jpa.ui.internal.wizards.entity;
 
 import static org.eclipse.jst.j2ee.internal.common.operations.INewJavaClassDataModelProperties.OPEN_IN_EDITOR;
@@ -45,36 +44,26 @@ public class EntityWizard
 	
 	protected IStructuredSelection selection;
 
-    /**
-     * Constructs the Entity wizard
-     * @param model the data model
-     */
     public EntityWizard(IDataModel model) {
 		super(model);
         setWindowTitle(EntityWizardMsg.ENTITY_WIZARD_TITLE);
         setDefaultPageImageDescriptor(JptJpaUiPlugin.getImageDescriptor(JptUiIcons.ENTITY_WIZ_BANNER));        
 	}    
  
-	/**
-	 * Empty constructor
-	 */
 	public EntityWizard(){
     	this(null);
     }
 	
-	/* Adds the two pages of the entity wizard 
-	 * @see org.eclipse.wst.common.frameworks.internal.datamodel.ui.DataModelWizard#doAddPages()
-	 */
 	@Override
 	protected void doAddPages() {
 		EntityClassWizardPage page1 = new EntityClassWizardPage(
 		        getDataModel(),
-		        "pageOne",
+		        "pageOne", //$NON-NLS-1$
 		        EntityWizardMsg.ENTITY_WIZARD_PAGE_DESCRIPTION,
 		        EntityWizardMsg.ENTITY_WIZARD_PAGE_TITLE);
 		page1.setProjectName(this.initialProjectName);
 		addPage(page1);
-		EntityFieldsWizardPage page2 = new EntityFieldsWizardPage(getDataModel(), "pageTwo");
+		EntityFieldsWizardPage page2 = new EntityFieldsWizardPage(getDataModel(), "pageTwo"); //$NON-NLS-1$
 		addPage(page2);
 	}
 
@@ -82,25 +71,21 @@ public class EntityWizard
 		return this.selection;
 	}
 
-	/* Return the default data model provider (EntityDataModelProvider in our case)
-	 * @see org.eclipse.wst.common.frameworks.internal.datamodel.ui.DataModelWizard#getDefaultProvider()
+	/**
+	 * EntityDataModelProvider
 	 */
 	@Override
 	protected IDataModelProvider getDefaultProvider() {
 		return new EntityDataModelProvider();
 	}
 
-	/* Check whether the mandatory information is set and wizard could finish
-	 * @see org.eclipse.wst.common.frameworks.internal.datamodel.ui.DataModelWizard#canFinish()
-	 */
 	@Override
 	public boolean canFinish() {
 		 return getDataModel().isValid();
 	}
 	
-	/* 
+	/**
 	 * Override the parent method in order to open the generated entity class in java editor
-	 * @see org.eclipse.wst.common.frameworks.internal.datamodel.ui.DataModelWizard#postPerformFinish()
 	 */
 	@Override
     protected void postPerformFinish() throws InvocationTargetException {      
@@ -118,7 +103,6 @@ public class EntityWizard
     /**
      * This method is intended for internal use only. It will open the file, sent as parameter
      * in its own java editor
-     * @param file who should be opened
      */
     private void openEditor(final IFile file) {
     	if (getDataModel().getBooleanProperty(OPEN_IN_EDITOR)) {
@@ -138,39 +122,32 @@ public class EntityWizard
     	}
     }
     
-    /* Implement the abstract method from IWorkbenchWizard
-     * @see org.eclipse.ui.IWorkbenchWizard#init(org.eclipse.ui.IWorkbench, org.eclipse.jface.viewers.IStructuredSelection)
-     */
-    public void init(IWorkbench workbench, IStructuredSelection selection) {
-    	this.selection = selection;
-    	this.initialProjectName = extractProjectName(selection);
+    public void init(IWorkbench workbench, IStructuredSelection sel) {
+    	this.selection = sel;
+    	this.initialProjectName = this.extractProjectName(sel.getFirstElement());
 		getDataModel();
 	}
-    
-    protected String extractProjectName(IStructuredSelection selection) {
-    	Object selectedObj = selection.getFirstElement();
-		if (selectedObj instanceof IResource) {
-			return ((IResource) selectedObj).getProject().getName();
-		}
-		if (selectedObj instanceof IJavaElement) {
-			return ((IJavaElement) selectedObj).getJavaProject().getProject().getName();
-		}
-		if (selectedObj instanceof JpaContextNode) {
-			return ((JpaContextNode) selectedObj).getJpaProject().getProject().getName();
-		}
-		
-		IResource resource = PlatformTools.getAdapter(selectedObj, IResource.class);
+
+    protected String extractProjectName(Object object) {
+    	if (object == null) {
+    		return null;
+    	}
+
+    	IResource resource = PlatformTools.getAdapter(object, IResource.class);
 		if (resource != null) {
 			return resource.getProject().getName();
 		}
-		IJavaElement javaElement = PlatformTools.getAdapter(selectedObj, IJavaElement.class);
+
+		IJavaElement javaElement = PlatformTools.getAdapter(object, IJavaElement.class);
 		if (javaElement != null) {
 			return javaElement.getJavaProject().getProject().getName();
 		}
-		JpaContextNode node = PlatformTools.getAdapter(selectedObj, JpaContextNode.class);
+
+		JpaContextNode node = PlatformTools.getAdapter(object, JpaContextNode.class);
 		if (node != null) {
 			return node.getJpaProject().getProject().getName();
 		}
+
 		return null;
 	}
 }
