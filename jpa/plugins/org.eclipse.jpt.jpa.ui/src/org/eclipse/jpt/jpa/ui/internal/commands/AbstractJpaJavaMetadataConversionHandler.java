c176bef5a0e3b0c96601f9ae675b1a6411b951ab
kmoore <kmoore>
2012-03-21 17:17:10 +0000

    Bug 374658 - In Package Explorer CCE converting java queries/generators/converters to XML - patch from Nan

--- jpa/plugins/org.eclipse.jpt.jpa.ui/src/org/eclipse/jpt/jpa/ui/internal/commands/AbstractJpaJavaMetadataConversionHandler.java
+++ jpa/plugins/org.eclipse.jpt.jpa.ui/src/org/eclipse/jpt/jpa/ui/internal/commands/AbstractJpaJavaMetadataConversionHandler.java
@@ -13,6 +13,7 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jpt.jpa.core.JpaProject;
 import org.eclipse.jpt.jpa.ui.JpaPlatformUi;
@@ -30,10 +31,11 @@ public abstract class AbstractJpaJavaMetadataConversionHandler extends AbstractH
 		IStructuredSelection selections = (IStructuredSelection)
 				HandlerUtil.getCurrentSelectionChecked(event);
 		
-		IProject project = (IProject)selections.iterator().next();
-		JpaProject jpaProject = (JpaProject)project.getAdapter(JpaProject.class);
-		converterJavaGlobalMetadata(jpaProject);
-		
+		Object selection = selections.iterator().next();
+		JpaProject jpaProject = getJpaProjectFromSelection(selection);
+		if (jpaProject != null) {
+			converterJavaGlobalMetadata(jpaProject);
+		}
 		return null;
 	}
 
@@ -43,5 +45,23 @@ public abstract class AbstractJpaJavaMetadataConversionHandler extends AbstractH
 	protected JpaPlatformUi getJpaPlatformUi(JpaProject project) {
 		String coreJpaPlatformId = project.getJpaPlatform().getId();
 		return JpaPlatformUiRegistry.instance().getJpaPlatformUi(coreJpaPlatformId);
+	}
+
+	protected JpaProject getJpaProjectFromSelection(Object selection) {
+		IProject project = getIProjectFromSelection(selection);
+		if (project != null) {
+			return (JpaProject) project.getAdapter(JpaProject.class);
+		}
+		return null;
+	}
+
+	protected IProject getIProjectFromSelection(Object selection) {
+		if (selection instanceof IProject) {
+			return (IProject) selection;
+		}
+		if (selection instanceof IJavaProject) {
+			return ((IJavaProject) selection).getProject();
+		}
+		return null;
 	}
 }
