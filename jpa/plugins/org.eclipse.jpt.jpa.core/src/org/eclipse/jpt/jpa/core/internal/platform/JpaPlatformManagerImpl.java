c78c2518633496d449c5d7463fae991385c91ace
kmoore <kmoore>
2012-04-27 15:23:10 +0000

    Bug 374638 - java.lang.IllegalStateException: Not started - catch Exception when building a JPA project and continue building the other JPA projects

--- jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/platform/JpaPlatformManagerImpl.java
+++ jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/platform/JpaPlatformManagerImpl.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2011 Oracle. All rights reserved.
+ * Copyright (c) 2006, 2012 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -231,7 +231,7 @@ public class JpaPlatformManagerImpl
 		String jpaPlatformId = JptJpaCorePlugin.getJpaPlatformId(project);
 		JpaPlatformDescriptionImpl platformDesc = this.jpaPlatformDescriptions.getItem(jpaPlatformId);
 		if (platformDesc == null) {
-			throw new IllegalArgumentException("Project does not have a recognized JPA platform.");
+			throw new IllegalArgumentException("Project does not have a recognized JPA platform: " + jpaPlatformId);
 		}
 		return platformDesc.buildJpaPlatform();
 	}
