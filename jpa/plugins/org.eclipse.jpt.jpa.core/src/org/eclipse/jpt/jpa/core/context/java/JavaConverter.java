56569e08caf8db9eed6467b873c7f0d092200948
kmoore <kmoore>
2012-07-18 17:51:50 +0000

    change JavaResourceAnnotatedElement.removeAnnotation(String) api to not check first if the annotation exists before removing it

--- jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/context/java/JavaConverter.java
+++ jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/context/java/JavaConverter.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2011 Oracle. All rights reserved.
+ * Copyright (c) 2008, 2012 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -134,7 +134,9 @@ public interface JavaConverter
 		}
 
 		public void removeConverterAnnotation(JavaResourceAttribute attribute) {
-			attribute.removeAnnotation(this.getAnnotationName());
+			if (attribute.getAnnotation(this.getAnnotationName()) != null) {
+				attribute.removeAnnotation(this.getAnnotationName());
+			}
 		}
 
 		protected Owner buildOwner() {
--- jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/jpa1/context/java/GenericJavaOrderable.java
+++ jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/jpa1/context/java/GenericJavaOrderable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2011 Oracle. All rights reserved.
+ * Copyright (c) 2009, 2012 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -278,7 +278,6 @@ public class GenericJavaOrderable
 
 	public void setOrderColumnOrdering(boolean orderColumnOrdering) {
 		if (orderColumnOrdering) {
-			this.removeOrderByAnnotation();
 			if (this.getOrderColumnAnnotation() == null) {
 				this.addOrderColumnAnnotation();
 			}
