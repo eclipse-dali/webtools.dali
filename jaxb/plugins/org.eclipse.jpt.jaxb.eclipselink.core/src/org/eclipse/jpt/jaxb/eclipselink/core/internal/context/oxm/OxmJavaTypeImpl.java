7b6288b99a81eeddc022333d999df56aa5229dc3
Paul Fullbright <paul.fullbright@oracle.com>
2012-10-08 14:38:02 -0400

    Added OxmJavaType/Impl

---
+++ jaxb/plugins/org.eclipse.jpt.jaxb.eclipselink.core/src/org/eclipse/jpt/jaxb/eclipselink/core/internal/context/oxm/OxmJavaTypeImpl.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ *  Copyright (c) 2012  Oracle. All rights reserved.
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Eclipse Public License v1.0, which accompanies this distribution
+ *  and is available at http://www.eclipse.org/legal/epl-v10.html
+ *  
+ *  Contributors: 
+ *  	Oracle - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jpt.jaxb.eclipselink.core.internal.context.oxm;
+
+import org.eclipse.jpt.jaxb.core.internal.context.AbstractJaxbContextNode;
+import org.eclipse.jpt.jaxb.eclipselink.core.context.oxm.OxmJavaType;
+import org.eclipse.jpt.jaxb.eclipselink.core.context.oxm.OxmXmlBindings;
+import org.eclipse.jpt.jaxb.eclipselink.core.resource.oxm.EJavaType;
+
+public class OxmJavaTypeImpl
+		extends AbstractJaxbContextNode
+		implements OxmJavaType {
+	
+	protected EJavaType eJavaType;
+	
+	public OxmJavaTypeImpl(OxmXmlBindings parent, EJavaType eJavaType) {
+		super(parent);
+		this.eJavaType = eJavaType;
+	}
+}
