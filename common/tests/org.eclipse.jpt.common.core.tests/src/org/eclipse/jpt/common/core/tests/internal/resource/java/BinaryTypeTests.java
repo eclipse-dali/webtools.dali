e70d269aa5020790673e74a6c4613d0d12c09b5e
Karen Butzke <karen.moore@oracle.com>
2012-10-09 16:39:35 -0400

    added copyright header for new classes

--- common/tests/org.eclipse.jpt.common.core.tests/src/org/eclipse/jpt/common/core/tests/internal/resource/java/BinaryTypeTests.java
+++ common/tests/org.eclipse.jpt.common.core.tests/src/org/eclipse/jpt/common/core/tests/internal/resource/java/BinaryTypeTests.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0, which accompanies this distribution
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ * 
+ * Contributors:
+ *     Oracle - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.jpt.common.core.tests.internal.resource.java;
 
 import java.io.File;
@@ -10,7 +19,7 @@ import org.eclipse.jpt.common.core.resource.java.JavaResourceMethod;
 import org.eclipse.jpt.common.core.resource.java.JavaResourceType;
 import org.eclipse.jpt.common.core.resource.java.NestableAnnotationDefinition;
 
-
+@SuppressWarnings("nls")
 public class BinaryTypeTests
 		extends JavaResourceModelTestCase {
 	
