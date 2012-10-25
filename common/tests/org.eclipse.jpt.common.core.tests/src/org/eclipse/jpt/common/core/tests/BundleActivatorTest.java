43afc205812739dc6b65426b0a10583d252b21ad
Brian Vosburgh <brian.vosburgh@oracle.com>
2012-10-25 15:28:26 -0400

    BundleActivatorTest would never actually fail(!) - fixed it

--- common/tests/org.eclipse.jpt.common.core.tests/src/org/eclipse/jpt/common/core/tests/BundleActivatorTest.java
+++ common/tests/org.eclipse.jpt.common.core.tests/src/org/eclipse/jpt/common/core/tests/BundleActivatorTest.java
@@ -9,8 +9,11 @@
  ******************************************************************************/
 package org.eclipse.jpt.common.core.tests;
 
+import java.util.Dictionary;
 import junit.framework.TestCase;
+import org.eclipse.jpt.common.utility.internal.ArrayTools;
 import org.eclipse.jpt.common.utility.internal.ObjectTools;
+import org.eclipse.jpt.common.utility.internal.transformer.TransformerAdapter;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
@@ -19,13 +22,9 @@ import org.osgi.framework.BundleReference;
 /**
  * This test case will verify that a class's "bundle activator" class
  * (i.e. its "plug-in" class - e.g. <code>JptCommonCorePlugin</code>)
- * has not been exported (accidentally).
+ * has not been (accidentally) exported.
  * <p>
  * Construct the test case with any class exported by the plug-in to be verified.
- * <p>
- * We use reflection(!) to fetch the bundle activator from the class's bundle
- * and then attempt to dynamically load the activator's class. This will fail
- * if the activator's class is <em>not</em> exported; which is what we want.
  */
 @SuppressWarnings("nls")
 public class BundleActivatorTest
@@ -55,15 +54,33 @@ public class BundleActivatorTest
 		if (this.bundle == null) {
 			fail("non-bundle class: " + this.clazz);
 		}
+		// get the list of exported packages...
+		Dictionary<String, String> headers = this.bundle.getHeaders();
+		String exportPackageHeader = headers.get("Export-Package");
+		String[] exportPackageEntries = exportPackageHeader.split(",");
+		String[] exportPackages = ArrayTools.transform(exportPackageEntries, PACKAGE_NAME_EXTRACTOR, String.class);
+
+		// ...get the activator class's package...
 		BundleContext bundleContext = this.bundle.getBundleContext();
 		// a little hackery:
 		BundleActivator activator = (BundleActivator) ObjectTools.get(bundleContext, "activator");
 		String activatorClassName = activator.getClass().getName();
-		try {
-			Class<?> activatorClass = activator.getClass().getClassLoader().loadClass(activatorClassName);
-			fail("activator class is exported: " + activatorClass.getName());
-		} catch (ClassNotFoundException ex) {
-			// success
+		Class<?> activatorClass = activator.getClass().getClassLoader().loadClass(activatorClassName);
+		String activatorPackage = activatorClass.getPackage().getName();
+
+		// ...and make sure it is not exported
+		if (ArrayTools.contains(exportPackages, activatorPackage)) {
+			fail("Bundle activator class is exported: " + activatorClassName);
+		}
+	}
+
+	private static final TransformerAdapter<String, String> PACKAGE_NAME_EXTRACTOR = new PackageNameExtractor();
+	/* CU private */ static class PackageNameExtractor
+		extends TransformerAdapter<String, String>
+	{
+		@Override
+		public String transform(String exportPackageEntry) {
+			return exportPackageEntry.split(";")[0];
 		}
 	}
 }
