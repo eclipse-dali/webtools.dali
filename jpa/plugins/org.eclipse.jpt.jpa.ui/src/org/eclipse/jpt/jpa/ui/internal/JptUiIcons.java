4abbf2b4034f735c3d6f49ae2c7242a7c3c87cfd
Pascal Filion <pascal.filion@oracle.com>
2012-10-30 11:03:20 -0400

    Added new icons for the upcoming enhancement to JPQL query content assist

--- jpa/plugins/org.eclipse.jpt.jpa.ui/src/org/eclipse/jpt/jpa/ui/internal/JptUiIcons.java
+++ jpa/plugins/org.eclipse.jpt.jpa.ui/src/org/eclipse/jpt/jpa/ui/internal/JptUiIcons.java
@@ -3,7 +3,7 @@
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
- * 
+ *
  * Contributors:
  *     Oracle - initial API and implementation
  ******************************************************************************/
@@ -13,25 +13,25 @@ package org.eclipse.jpt.jpa.ui.internal;
 public class JptUiIcons {
 
 	// **************** General JPA icons **************************************
-	
+
 	public static final String JPA_CONTENT = "full/obj16/jpa-content";
-	
+
 	public static final String JPA_FILE = "full/obj16/jpa-file";
-	
+
 	public static final String JAR_FILE = "full/obj16/jpa-jar-file";
-	
+
 	public static final String WARNING = "full/obj16/warning";
-	
-	
+
+
 	// **************** Wizard icons *******************************************
-	
+
 	public static final String JPA_WIZ_BANNER = "full/wizban/new_jpa_prj_wiz";
-	
+
 	public static final String ENTITY_WIZ_BANNER = "full/wizban/new_entity_wizban";
-	
+
 	public static final String JPA_FILE_WIZ_BANNER = "full/wizban/new_jpa_file_wizban";
-	
-	
+
+
 	// **************** Persistence icons **************************************
 
 	public static final String PERSISTENCE = "full/obj16/persistence";
@@ -41,7 +41,7 @@ public class JptUiIcons {
 	public static final String MAPPING_FILE_REF = "full/obj16/jpa-file";
 
 	public static final String CLASS_REF = "full/obj16/null-type-mapping";
-	
+
 	public static final String JAR_FILE_REF = "full/obj16/jpa-jar-file";
 
 
@@ -51,38 +51,50 @@ public class JptUiIcons {
 
 
 	// **************** Orm/Java common icons **********************************
-	
+
 	public static final String ENTITY = "full/obj16/entity";
-	
+
 	public static final String EMBEDDABLE = "full/obj16/embeddable";
-	
+
 	public static final String MAPPED_SUPERCLASS = "full/obj16/mapped-superclass";
-	
+
 	public static final String NULL_TYPE_MAPPING = "full/obj16/null-type-mapping";
-	
+
 	public static final String ID = "full/obj16/id";
-	
+
 	public static final String EMBEDDED_ID = "full/obj16/embedded-id";
-	
+
 	public static final String BASIC = "full/obj16/basic";
-	
+
 	public static final String VERSION = "full/obj16/version";
-	
+
 	public static final String MANY_TO_ONE = "full/obj16/many-to-one";
-	
+
 	public static final String ONE_TO_MANY = "full/obj16/one-to-many";
-	
+
 	public static final String ONE_TO_ONE = "full/obj16/one-to-one";
-	
+
 	public static final String MANY_TO_MANY = "full/obj16/many-to-many";
-	
+
 	public static final String ELEMENT_COLLECTION = "full/obj16/element-collection";
-	
+
 	public static final String EMBEDDED = "full/obj16/embedded";
-	
+
 	public static final String TRANSIENT = "full/obj16/transient";
-	
+
 	public static final String NULL_ATTRIBUTE_MAPPING = "full/obj16/null-attribute-mapping";
+
+
+	// **************** Class metadata icons **********************************
+
+	public static final String ENUM_REF = "full/obj16/enum_obj";
+
+
+	// **************** Database icons **********************************
+
+	public static final String COLUMN = "full/obj16/column";
+
+	public static final String TABLE = "full/obj16/table";
 
 
 	// **************** Content Assist icons **********************************
