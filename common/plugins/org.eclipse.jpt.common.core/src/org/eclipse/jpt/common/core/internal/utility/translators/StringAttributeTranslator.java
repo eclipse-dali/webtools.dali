c410611f9827c189c7e4f620e94f5104dc7768bb
Paul Fullbright <paul.fullbright@oracle.com>
2013-02-04 11:37:24 -0500

    maintains null values

---
+++ common/plugins/org.eclipse.jpt.common.core/src/org/eclipse/jpt/common/core/internal/utility/translators/StringAttributeTranslator.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ *  Copyright (c) 2013  Oracle. All rights reserved.
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Eclipse Public License v1.0, which accompanies this distribution
+ *  and is available at http://www.eclipse.org/legal/epl-v10.html
+ *  
+ *  Contributors: 
+ *  	Oracle - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.jpt.common.core.internal.utility.translators;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.wst.common.internal.emf.resource.Translator;
+
+/**
+ * Simple fix for String attribute translation:  maps null to null instead of ""
+ */
+public class StringAttributeTranslator
+		extends Translator {
+	
+	public StringAttributeTranslator(String domNameAndPath, EStructuralFeature aFeature, int style) {
+		super(domNameAndPath, aFeature, Translator.DOM_ATTRIBUTE | style);
+	}
+	
+	@Override
+	public Object convertStringToValue(String strValue, EObject owner) {
+		if (strValue == null) {
+			return null;
+		}
+		return super.convertStringToValue(strValue, owner);
+	}
+}
