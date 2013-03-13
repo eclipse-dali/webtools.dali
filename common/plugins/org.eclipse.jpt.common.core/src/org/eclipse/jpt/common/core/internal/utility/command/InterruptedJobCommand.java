f5443187dee369e320e9cd5cd27ed4b7b67c515c
Brian Vosburgh <brian.vosburgh@oracle.com>
2013-03-13 14:35:20 -0400

    clean up InterruptibleJobCommand

---
+++ common/plugins/org.eclipse.jpt.common.core/src/org/eclipse/jpt/common/core/internal/utility/command/InterruptedJobCommand.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2013 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0, which accompanies this distribution
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ * 
+ * Contributors:
+ *     Oracle - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.jpt.common.core.internal.utility.command;
+
+import java.io.Serializable;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jpt.common.core.utility.command.InterruptibleJobCommand;
+import org.eclipse.jpt.common.utility.internal.ObjectTools;
+
+/**
+ * Singleton implementation of the interruptible job command interface that
+ * will throw an {@link InterruptedException exception} when executed.
+ */
+public final class InterruptedJobCommand
+	implements InterruptibleJobCommand, Serializable
+{
+	public static final InterruptibleJobCommand INSTANCE = new InterruptedJobCommand();
+
+	public static InterruptibleJobCommand instance() {
+		return INSTANCE;
+	}
+
+	// ensure single instance
+	private InterruptedJobCommand() {
+		super();
+	}
+
+	// throw an exception
+	public IStatus execute(IProgressMonitor monitor) throws InterruptedException {
+		throw new InterruptedException();
+	}
+
+	@Override
+	public String toString() {
+		return ObjectTools.singletonToString(this);
+	}
+
+	private static final long serialVersionUID = 1L;
+	private Object readResolve() {
+		// replace this object with the singleton
+		return INSTANCE;
+	}
+}
--- common/plugins/org.eclipse.jpt.common.core/src/org/eclipse/jpt/common/core/utility/command/InterruptibleJobCommand.java
+++ common/plugins/org.eclipse.jpt.common.core/src/org/eclipse/jpt/common/core/utility/command/InterruptibleJobCommand.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Oracle. All rights reserved.
+ * Copyright (c) 2012, 2013 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -9,10 +9,8 @@
  ******************************************************************************/
 package org.eclipse.jpt.common.core.utility.command;
 
-import java.io.Serializable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.jpt.common.utility.internal.ObjectTools;
 
 /**
  * Simple interface for implementing the GOF Command design pattern
@@ -47,35 +45,4 @@ public interface InterruptibleJobCommand {
 	 * @see org.eclipse.core.runtime.jobs.Job#run(IProgressMonitor monitor)
 	 */
 	IStatus execute(IProgressMonitor monitor) throws InterruptedException;
-
-
-	/**
-	 * Singleton implementation of the interruptible job command interface that
-	 * will throw an interrupted exception when executed.
-	 */
-	final class Interrupted
-		implements InterruptibleJobCommand, Serializable
-	{
-		public static final InterruptibleJobCommand INSTANCE = new Interrupted();
-		public static InterruptibleJobCommand instance() {
-			return INSTANCE;
-		}
-		// ensure single instance
-		private Interrupted() {
-			super();
-		}
-		// throw an exception
-		public IStatus execute(IProgressMonitor monitor) throws InterruptedException {
-			throw new InterruptedException();
-		}
-		@Override
-		public String toString() {
-			return ObjectTools.singletonToString(this);
-		}
-		private static final long serialVersionUID = 1L;
-		private Object readResolve() {
-			// replace this object with the singleton
-			return INSTANCE;
-		}
-	}
 }
