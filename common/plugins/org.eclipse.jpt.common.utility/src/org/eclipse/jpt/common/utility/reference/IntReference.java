15c3cb7b22ae26c86663ac879ec83cb0aa15f28a
Brian Vosburgh <brian.vosburgh@oracle.com>
2013-03-13 15:58:49 -0400

    clean up IntReference

--- common/plugins/org.eclipse.jpt.common.utility/src/org/eclipse/jpt/common/utility/reference/IntReference.java
+++ common/plugins/org.eclipse.jpt.common.utility/src/org/eclipse/jpt/common/utility/reference/IntReference.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2012 Oracle. All rights reserved.
+ * Copyright (c) 2010, 2013 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -10,8 +10,7 @@
 package org.eclipse.jpt.common.utility.reference;
 
 /**
- * Interface for a container for holding an <code>int</code> that cannot be
- * changed by clients.
+ * Interface for a container for holding an <code>int</code>.
  * <p>
  * Provisional API: This interface is part of an interim API that is still
  * under development and expected to change significantly before reaching
