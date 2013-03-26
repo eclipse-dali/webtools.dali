86241b0f2d0821524cfab473d3794d3eec8eed8c
Brian Vosburgh <brian.vosburgh@oracle.com>
2013-03-26 14:34:23 -0400

    clean up GenericMetamodelSynchronizer2_0

--- jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/jpa2/GenericMetamodelSynchronizer2_0.java
+++ jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/internal/jpa2/GenericMetamodelSynchronizer2_0.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2012 Oracle. All rights reserved.
+ * Copyright (c) 2009, 2013 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -223,6 +223,8 @@ public class GenericMetamodelSynchronizer2_0
 		IndentingPrintWriter pw = new IndentingPrintWriter(sw, this.getLineSeparator());
 		this.printPackageAndImportsOn(pw, bodySourceWriter);
 		pw.print(bodySourceWriter.getSource());
+		pw.close();
+		bodySourceWriter.close();
 		return sw.toString();
 	}
 
@@ -434,5 +436,4 @@ public class GenericMetamodelSynchronizer2_0
 	protected String getLineSeparator() {
 		return PlatformTools.getNewTextFileLineDelimiter();
 	}
-
 }
