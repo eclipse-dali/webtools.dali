5def8293773ae341cc4d070ffa33fa529fa7a0ad
Brian Vosburgh <brian.vosburgh@oracle.com>
2013-03-26 18:57:10 -0400

    rename BinaryEclipseLinkConvertAnnotation to EclipseLinkBinaryConvertAnnotation

--- jpa/plugins/org.eclipse.jpt.jpa.eclipselink.core/src/org/eclipse/jpt/jpa/eclipselink/core/internal/resource/java/EclipseLinkConvertAnnotationDefinition.java
+++ jpa/plugins/org.eclipse.jpt.jpa.eclipselink.core/src/org/eclipse/jpt/jpa/eclipselink/core/internal/resource/java/EclipseLinkConvertAnnotationDefinition.java
@@ -14,7 +14,7 @@ import org.eclipse.jpt.common.core.resource.java.Annotation;
 import org.eclipse.jpt.common.core.resource.java.AnnotationDefinition;
 import org.eclipse.jpt.common.core.resource.java.JavaResourceAnnotatedElement;
 import org.eclipse.jpt.common.core.utility.jdt.AnnotatedElement;
-import org.eclipse.jpt.jpa.eclipselink.core.internal.resource.java.binary.BinaryEclipseLinkConvertAnnotation;
+import org.eclipse.jpt.jpa.eclipselink.core.internal.resource.java.binary.EclipseLinkBinaryConvertAnnotation;
 import org.eclipse.jpt.jpa.eclipselink.core.internal.resource.java.source.SourceEclipseLinkConvertAnnotation;
 import org.eclipse.jpt.jpa.eclipselink.core.resource.java.ConvertAnnotation;
 
@@ -50,7 +50,7 @@ public class EclipseLinkConvertAnnotationDefinition
 	}
 
 	public Annotation buildAnnotation(JavaResourceAnnotatedElement parent, IAnnotation jdtAnnotation) {
-		return new BinaryEclipseLinkConvertAnnotation(parent, jdtAnnotation);
+		return new EclipseLinkBinaryConvertAnnotation(parent, jdtAnnotation);
 	}
 
 	public String getAnnotationName() {
--- jpa/plugins/org.eclipse.jpt.jpa.eclipselink.core/src/org/eclipse/jpt/jpa/eclipselink/core/internal/resource/java/binary/EclipseLinkBinaryConvertAnnotation.java
+++ jpa/plugins/org.eclipse.jpt.jpa.eclipselink.core/src/org/eclipse/jpt/jpa/eclipselink/core/internal/resource/java/binary/EclipseLinkBinaryConvertAnnotation.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2012 Oracle. All rights reserved.
+ * Copyright (c) 2009, 2013 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -17,11 +17,11 @@ import org.eclipse.jpt.jpa.eclipselink.core.resource.java.ConvertAnnotation;
 /**
  * <code>org.eclipse.persistence.annotations.Convert</code>
  */
-public final class BinaryEclipseLinkConvertAnnotation
+public final class EclipseLinkBinaryConvertAnnotation
 	extends BinaryEclipseLinkBaseConvertAnnotation
 	implements ConvertAnnotation
 {
-	public BinaryEclipseLinkConvertAnnotation(JavaResourceAnnotatedElement parent, IAnnotation jdtAnnotation) {
+	public EclipseLinkBinaryConvertAnnotation(JavaResourceAnnotatedElement parent, IAnnotation jdtAnnotation) {
 		super(parent, jdtAnnotation);
 	}
 
