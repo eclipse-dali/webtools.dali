3c552473cb9129701368c2234f6df1187528fc70
Brian Vosburgh <brian.vosburgh@oracle.com>
2013-03-26 12:48:02 -0400

    add package-info.java to .internal.bundleexclude pacakge

---
+++ common/plugins/org.eclipse.jpt.common.core/src/org/eclipse/jpt/common/core/internal/bundleexclude/package-info.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Oracle. All rights reserved.
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0, which accompanies this distribution
+ * and is available at http://www.eclipse.org/legal/epl-v10.html.
+ * 
+ * Contributors:
+ *     Oracle - initial API and implementation
+ ******************************************************************************/
+
+/**
+ * Classes in this package can be loaded by the OSGi classloader without
+ * activating the plug-in.
+ * <p>
+ * See <code>./META-INF/MANIFEST.MF:Bundle-ActivationPolicy</code>
+ */
+package org.eclipse.jpt.common.core.internal.bundleexclude;
