922791b7bea6c9324ff9962ac30247bbc2c6a7cf
Brian Vosburgh <brian.vosburgh@oracle.com>
2013-03-28 13:43:09 -0400

    rename Generic2_0JpaPlatformUiProvider to GenericJpaPlatformUiProvider2_0

--- jpa/plugins/org.eclipse.jpt.jpa.ui/src/org/eclipse/jpt/jpa/ui/internal/jpa2/GenericJpaPlatformUiProvider2_0.java
+++ jpa/plugins/org.eclipse.jpt.jpa.ui/src/org/eclipse/jpt/jpa/ui/internal/jpa2/GenericJpaPlatformUiProvider2_0.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2012 Oracle. All rights reserved.
+ * Copyright (c) 2009, 2013 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -23,11 +23,12 @@ import org.eclipse.jpt.jpa.ui.internal.persistence.PersistenceXmlUiDefinition;
  * All the state in the JPA platform should be "static" (i.e. unchanging once
  * it is initialized).
  */
-public class Generic2_0JpaPlatformUiProvider extends AbstractJpaPlatformUiProvider
+public class GenericJpaPlatformUiProvider2_0
+	extends AbstractJpaPlatformUiProvider
 {
 
 	// singleton
-	private static final JpaPlatformUiProvider INSTANCE = new Generic2_0JpaPlatformUiProvider();
+	private static final JpaPlatformUiProvider INSTANCE = new GenericJpaPlatformUiProvider2_0();
 
 	/**
 	 * Return the singleton.
@@ -39,7 +40,7 @@ public class Generic2_0JpaPlatformUiProvider extends AbstractJpaPlatformUiProvid
 	/**
 	 * Ensure single instance.
 	 */
-	private Generic2_0JpaPlatformUiProvider() {
+	private GenericJpaPlatformUiProvider2_0() {
 		super();
 	}
 
