a1c28d3c28fc945494418ba2aa9ef34ad194eeac
Brian Vosburgh <brian.vosburgh@oracle.com>
2013-04-17 16:25:17 -0400

    rename AddVirtualAttributeHandler to EclipseLinkAddVirtualAttributeHandler

--- jpa/plugins/org.eclipse.jpt.jpa.eclipselink.ui/src/org/eclipse/jpt/jpa/eclipselink/ui/internal/handlers/EclipseLinkAddVirtualAttributeHandler.java
+++ jpa/plugins/org.eclipse.jpt.jpa.eclipselink.ui/src/org/eclipse/jpt/jpa/eclipselink/ui/internal/handlers/EclipseLinkAddVirtualAttributeHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Oracle. All rights reserved.
+ * Copyright (c) 2012, 2013 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -27,7 +27,7 @@ import org.eclipse.ui.handlers.HandlerUtil;
  * <p>
  * See <code>org.eclipse.jpt.jpa.eclipselink.ui/plugin.xml</code>.
  */
-public class AddVirtualAttributeHandler
+public class EclipseLinkAddVirtualAttributeHandler
 	extends AbstractHandler
 {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
