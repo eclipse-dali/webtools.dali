ca2ffdb551a3627f674ed25b1c9441c98d43ddf9
Brian Vosburgh <brian.vosburgh@oracle.com>
2013-04-17 16:23:53 -0400

    rename NewEclipseLinkMappingFileHandler to EclipseLinkNewMappingFileHandler

--- jpa/plugins/org.eclipse.jpt.jpa.eclipselink.ui/src/org/eclipse/jpt/jpa/eclipselink/ui/internal/handlers/EclipseLinkNewMappingFileHandler.java
+++ jpa/plugins/org.eclipse.jpt.jpa.eclipselink.ui/src/org/eclipse/jpt/jpa/eclipselink/ui/internal/handlers/EclipseLinkNewMappingFileHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012 Oracle. All rights reserved.
+ * Copyright (c) 2012, 2013 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -21,7 +21,8 @@ import org.eclipse.ui.INewWizard;
 /**
  *  NewEclipseLinkMappingFileHandler
  */
-public class NewEclipseLinkMappingFileHandler extends AbstractHandler
+public class EclipseLinkNewMappingFileHandler
+	extends AbstractHandler
 {
 
 	public Object execute(ExecutionEvent event) throws ExecutionException {
