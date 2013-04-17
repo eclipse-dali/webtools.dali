64f7268ac6879fd366621a4bf174ec84f4a32260
Brian Vosburgh <brian.vosburgh@oracle.com>
2013-04-17 17:13:15 -0400

    rename EclipseLink2_1JpaPlatformUiFactory to EclipseLinkJpaPlatformUiFactory2_1

--- jpa/plugins/org.eclipse.jpt.jpa.eclipselink.ui/src/org/eclipse/jpt/jpa/eclipselink/ui/internal/platform/EclipseLinkJpaPlatformUiFactory2_1.java
+++ jpa/plugins/org.eclipse.jpt.jpa.eclipselink.ui/src/org/eclipse/jpt/jpa/eclipselink/ui/internal/platform/EclipseLinkJpaPlatformUiFactory2_1.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2012 Oracle. All rights reserved.
+ * Copyright (c) 2010, 2013 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -11,13 +11,13 @@ package org.eclipse.jpt.jpa.eclipselink.ui.internal.platform;
 
 import org.eclipse.jpt.jpa.ui.JpaPlatformUi;
 
-public class EclipseLink2_1JpaPlatformUiFactory
+public class EclipseLinkJpaPlatformUiFactory2_1
 	extends EclipseLink2_0JpaPlatformUiFactory
 {
 	/**
 	 * Zero arg constructor for extension point
 	 */
-	public EclipseLink2_1JpaPlatformUiFactory() {
+	public EclipseLinkJpaPlatformUiFactory2_1() {
 		super();
 	}
 
