1fa76d1f965aefa81b669362b72a3530b1944e1b
Brian Vosburgh <brian.vosburgh@oracle.com>
2013-04-17 17:31:45 -0400

    rename EclipseLinkPersistenceUnitOptions2_0EditorPageDefinition to EclipseLinkPersistenceUnitOptionsEditorPageDefinition2_0

--- jpa/plugins/org.eclipse.jpt.jpa.eclipselink.ui/src/org/eclipse/jpt/jpa/eclipselink/ui/internal/persistence/EclipseLinkPersistenceXmlUiDefinition2_0.java
+++ jpa/plugins/org.eclipse.jpt.jpa.eclipselink.ui/src/org/eclipse/jpt/jpa/eclipselink/ui/internal/persistence/EclipseLinkPersistenceXmlUiDefinition2_0.java
@@ -46,7 +46,7 @@ public class EclipseLinkPersistenceXmlUiDefinition2_0
 		definitions.add(EclipseLinkPersistenceUnitConnectionEditorPageDefinition.instance());
 		definitions.add(EclipseLinkPersistenceUnitCustomization2_0EditorPageDefinition.instance());
 		definitions.add(EclipseLinkPersistenceUnitCaching2_0EditorPageDefinition.instance());
-		definitions.add(EclipseLinkPersistenceUnitOptions2_0EditorPageDefinition.instance());
+		definitions.add(EclipseLinkPersistenceUnitOptionsEditorPageDefinition2_0.instance());
 		definitions.add(PersistenceUnitPropertiesEditorPageDefinition.instance());
 	}
 
