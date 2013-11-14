ec709cad52671c4042df918f1eed6e7b5711dcde
Nan Li <nan.n.li@oracle.com>
2013-11-14 16:50:29 -0600

    Bug 421555 - Schema information not displayed for SQL database

--- jpa/plugins/org.eclipse.jpt.jpa.db/src/org/eclipse/jpt/jpa/db/internal/driver/SimpleCatalogStrategy.java
+++ jpa/plugins/org.eclipse.jpt.jpa.db/src/org/eclipse/jpt/jpa/db/internal/driver/SimpleCatalogStrategy.java
@@ -31,16 +31,36 @@ class SimpleCatalogStrategy
 		this.database = database;
 	}
 
+	@SuppressWarnings("unchecked")
 	public boolean supportsCatalogs() {
+		// DTP allows for optional support of catalogs in extensions
+		List<Catalog> catalogs = this.database.getCatalogs();
+		if ((catalogs == null) || catalogs.isEmpty()) {
+			return false;
+		}
+		
 		return true;
 	}
 
 	@SuppressWarnings("unchecked")
 	public List<Catalog> getCatalogs() {
-		return this.database.getCatalogs();
+		List<Catalog> catalogs = this.database.getCatalogs();
+		// DTP allows for optional support of catalogs in extensions
+		if ((catalogs == null) || catalogs.isEmpty()) {
+			return Collections.emptyList();
+		}
+		
+		return catalogs;
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<Schema> getSchemas() {
+		List<Catalog> catalogs = this.database.getCatalogs();
+		// if there are no catalogs, the database must hold the schemata directly
+		if ((catalogs == null) || catalogs.isEmpty()) {
+			return this.database.getSchemas();
+		}
+		
 		return Collections.emptyList();
 	}
 }
