0cda946b3d142c0cfd92035f54e8aa2115d787e4
Brian Vosburgh <brian.vosburgh@oracle.com>
2016-02-04 11:24:51 -0500

    fix compiler warnings in ThreadLocalInterruptibleClosure

--- common/plugins/org.eclipse.jpt.common.utility/src/org/eclipse/jpt/common/utility/internal/closure/ThreadLocalInterruptibleClosure.java
+++ common/plugins/org.eclipse.jpt.common.utility/src/org/eclipse/jpt/common/utility/internal/closure/ThreadLocalInterruptibleClosure.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2013 Oracle. All rights reserved.
+ * Copyright (c) 2008, 2016 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -30,7 +30,7 @@ public class ThreadLocalInterruptibleClosure<A>
 	}
 
 	private ThreadLocal<InterruptibleClosure<? super A>> buildThreadLocal() {
-		return new ThreadLocal<InterruptibleClosure<? super A>>();
+		return new ThreadLocal<>();
 	}
 
 	public void execute(A argument) throws InterruptedException {
