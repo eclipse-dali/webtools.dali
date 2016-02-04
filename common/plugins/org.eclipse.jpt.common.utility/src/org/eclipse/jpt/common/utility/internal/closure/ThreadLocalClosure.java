594e87575cc650caf305e5208dde2d6d39291251
Brian Vosburgh <brian.vosburgh@oracle.com>
2016-02-04 11:24:21 -0500

    fix compiler warnings in ThreadLocalClosure

--- common/plugins/org.eclipse.jpt.common.utility/src/org/eclipse/jpt/common/utility/internal/closure/ThreadLocalClosure.java
+++ common/plugins/org.eclipse.jpt.common.utility/src/org/eclipse/jpt/common/utility/internal/closure/ThreadLocalClosure.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2013 Oracle. All rights reserved.
+ * Copyright (c) 2008, 2016 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -36,7 +36,7 @@ public class ThreadLocalClosure<A>
 	}
 
 	private ThreadLocal<Closure<? super A>> buildThreadLocal() {
-		return new ThreadLocal<Closure<? super A>>();
+		return new ThreadLocal<>();
 	}
 
 	public void execute(A argument) {
