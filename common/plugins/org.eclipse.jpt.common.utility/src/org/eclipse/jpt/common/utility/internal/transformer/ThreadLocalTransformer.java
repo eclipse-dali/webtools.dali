681cc1d90f684af6feced52c21429178570e33d3
Brian Vosburgh <brian.vosburgh@oracle.com>
2016-02-04 13:48:25 -0500

    fix compiler warnings in ThreadLocalTransformer

--- common/plugins/org.eclipse.jpt.common.utility/src/org/eclipse/jpt/common/utility/internal/transformer/ThreadLocalTransformer.java
+++ common/plugins/org.eclipse.jpt.common.utility/src/org/eclipse/jpt/common/utility/internal/transformer/ThreadLocalTransformer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2013 Oracle. All rights reserved.
+ * Copyright (c) 2008, 2016 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -37,7 +37,7 @@ public class ThreadLocalTransformer<I, O>
 	}
 
 	private ThreadLocal<Transformer<? super I, ? extends O>> buildThreadLocal() {
-		return new ThreadLocal<Transformer<? super I, ? extends O>>();
+		return new ThreadLocal<>();
 	}
 
 	public O transform(I input) {
