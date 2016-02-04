06144d64582ad4db8b2ba25df74ef06e7db314d7
Brian Vosburgh <brian.vosburgh@oracle.com>
2016-02-04 12:31:49 -0500

    fix compiler warnings in utility.internal.exception

--- common/plugins/org.eclipse.jpt.common.utility/src/org/eclipse/jpt/common/utility/internal/exception/AbstractCompositeExceptionHandler.java
+++ common/plugins/org.eclipse.jpt.common.utility/src/org/eclipse/jpt/common/utility/internal/exception/AbstractCompositeExceptionHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2013 Oracle. All rights reserved.
+ * Copyright (c) 2012, 2016 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0, which accompanies this distribution
  * and is available at http://www.eclipse.org/legal/epl-v10.html.
@@ -22,13 +22,14 @@ import org.eclipse.jpt.common.utility.internal.iterable.IterableTools;
 public abstract class AbstractCompositeExceptionHandler<H extends ExceptionHandler>
 	implements ExceptionHandler
 {
-	private final Vector<H> exceptionHandlers = new Vector<H>();
+	private final Vector<H> exceptionHandlers = new Vector<>();
 
 
 	protected AbstractCompositeExceptionHandler() {
 		super();
 	}
 
+	@SafeVarargs
 	protected AbstractCompositeExceptionHandler(H... exceptionHandlers) {
 		super();
 		CollectionTools.addAll(this.exceptionHandlers, exceptionHandlers);
