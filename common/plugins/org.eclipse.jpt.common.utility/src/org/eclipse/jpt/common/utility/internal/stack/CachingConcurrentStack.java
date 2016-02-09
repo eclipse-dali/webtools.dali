2d621b758345525c3e4a55aa951b48d82908675d
Brian Vosburgh <brian.vosburgh@oracle.com>
2016-02-09 09:57:14 -0500

    test and fix(!) CachingConcurrentStack

--- common/plugins/org.eclipse.jpt.common.utility/src/org/eclipse/jpt/common/utility/internal/stack/CachingConcurrentStack.java
+++ common/plugins/org.eclipse.jpt.common.utility/src/org/eclipse/jpt/common/utility/internal/stack/CachingConcurrentStack.java
@@ -11,7 +11,6 @@ package org.eclipse.jpt.common.utility.internal.stack;
 
 import java.util.ArrayList;
 import java.util.EmptyStackException;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.atomic.AtomicStampedReference;
 import org.eclipse.jpt.common.utility.internal.ObjectTools;
 import org.eclipse.jpt.common.utility.stack.Stack;
@@ -34,7 +33,7 @@ public class CachingConcurrentStack<E>
 {
 	private final AtomicStampedReference<Node<E>> headRef = new AtomicStampedReference<>(null, 0);
 
-	private final AtomicReference<Node<E>> cacheHeadRef = new AtomicReference<>(null);
+	private final AtomicStampedReference<Node<E>> cacheHeadRef = new AtomicStampedReference<>(null, 0);
 
 	/**
 	 * Construct an empty stack.
@@ -52,13 +51,14 @@ public class CachingConcurrentStack<E>
 		}
 		Node<E> newHead = this.buildNode(element);
 		Node<E> oldHead = null;
-		int[] stampHolder = new int[1];
-		int stamp = -1;
+		int[] oldStampHolder = new int[1];
+		int newStamp = -1;
 		do {
-			oldHead = this.headRef.get(stampHolder);
+			oldHead = this.headRef.get(oldStampHolder);
 			newHead.next = oldHead;
-			stamp = stampHolder[0];
-		} while ( ! this.headRef.compareAndSet(oldHead, newHead, stampHolder[0], ++stamp));
+			newStamp = oldStampHolder[0];
+			newStamp++;
+		} while ( ! this.headRef.compareAndSet(oldHead, newHead, oldStampHolder[0], newStamp));
 	}
 
 	/**
@@ -67,16 +67,17 @@ public class CachingConcurrentStack<E>
 	public E pop() {
 		Node<E> oldHead = null;
 		Node<E> newHead = null;
-		int[] stampHolder = new int[1];
-		int stamp = -1;
+		int[] oldStampHolder = new int[1];
+		int newStamp = -1;
 		do {
-			oldHead = this.headRef.get(stampHolder);
+			oldHead = this.headRef.get(oldStampHolder);
 			if (oldHead == null) {
 				return null;
 			}
 			newHead = oldHead.next;
-			stamp = stampHolder[0];
-		} while ( ! this.headRef.compareAndSet(oldHead, newHead, stampHolder[0], ++stamp));
+			newStamp = oldStampHolder[0];
+			newStamp++;
+		} while ( ! this.headRef.compareAndSet(oldHead, newHead, oldStampHolder[0], newStamp));
 		E element = oldHead.element;
 		this.release(oldHead);
 		return element;
@@ -86,36 +87,41 @@ public class CachingConcurrentStack<E>
 	 * @return <code>null</code> if the stack is empty
 	 */
 	public E peek() {
-		Node<E> head = this.headRef.get(DUMMY_STAMP_HOLDER);
+		int[] stampHolder = new int[1];
+		Node<E> head = this.headRef.get(stampHolder);
 		return (head == null) ? null : head.element;
 	}
 
 	public boolean isEmpty() {
-		return this.headRef.get(DUMMY_STAMP_HOLDER) == null;
+		int[] stampHolder = new int[1];
+		return this.headRef.get(stampHolder) == null;
 	}
 
 	// may be inaccurate
 	@Override
 	public String toString() {
+		int[] stampHolder = new int[1];
 		ArrayList<E> nodes = new ArrayList<>();
-		for (Node<E> node = this.headRef.get(DUMMY_STAMP_HOLDER); node != null; node = node.next) {
+		for (Node<E> node = this.headRef.get(stampHolder); node != null; node = node.next) {
 			nodes.add(node.element);
 		}
 		return nodes.toString();
 	}
 
-	private static final int[] DUMMY_STAMP_HOLDER = new int[1];
-
 	private Node<E> buildNode(E element) {
 		Node<E> oldHead = null;
 		Node<E> newHead = null;
+		int[] oldStampHolder = new int[1];
+		int newStamp = -1;
 		do {
-			oldHead = this.cacheHeadRef.get();
+			oldHead = this.cacheHeadRef.get(oldStampHolder);
 			if (oldHead == null) {
 				return new Node<>(element);
 			}
 			newHead = oldHead.next;
-		} while ( ! this.cacheHeadRef.compareAndSet(oldHead, newHead));
+			newStamp = oldStampHolder[0];
+			newStamp++;
+		} while ( ! this.cacheHeadRef.compareAndSet(oldHead, newHead, oldStampHolder[0], newStamp));
 		oldHead.element = element;
 		return oldHead;
 	}
@@ -123,18 +129,22 @@ public class CachingConcurrentStack<E>
 	private void release(Node<E> node) {
 		node.element = null; // allow GC to work
 		Node<E> oldHead = null;
+		int[] oldStampHolder = new int[1];
+		int newStamp = -1;
 		do {
-			oldHead = this.cacheHeadRef.get();
+			oldHead = this.cacheHeadRef.get(oldStampHolder);
 			node.next = oldHead;
-		} while ( ! this.cacheHeadRef.compareAndSet(oldHead, node));
+			newStamp = oldStampHolder[0];
+			newStamp++;
+		} while ( ! this.cacheHeadRef.compareAndSet(oldHead, node, oldStampHolder[0], newStamp));
 	}
 
 
 	// ********** Node **********
 
 	private static final class Node<E> {
-		E element;
-		Node<E> next = null;
+		volatile E element;
+		volatile Node<E> next = null;
 
 		Node(E element) {
 			super();
