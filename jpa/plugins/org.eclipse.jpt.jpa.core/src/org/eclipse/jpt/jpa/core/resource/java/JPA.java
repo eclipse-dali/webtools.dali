08c7719d4f8fc7e7030eea526b271fe1eedcadfe
Lakshminarayana Nekkanti <narayana.nekkanti@gmail.com>
2025-06-09 20:10:16 +0530

    The "New JPA entity" wizard generates the wrong import package when the project uses JPA 3.0 or greater. #13
    Change-Id: I8566c8311aae250e46ce357b1d3266b55904e2b0

--- jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/resource/java/JPA.java
+++ jpa/plugins/org.eclipse.jpt.jpa.core/src/org/eclipse/jpt/jpa/core/resource/java/JPA.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2011 Oracle. All rights reserved.
+ * Copyright (c) 2006, 2025 Oracle. All rights reserved.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0, which accompanies this distribution
  * and is available at https://www.eclipse.org/legal/epl-2.0/.
@@ -25,6 +25,7 @@ package org.eclipse.jpt.jpa.core.resource.java;
 public interface JPA {
 
 	// JPA package
+	String JAKARTA_PACKAGE = "jakarta.persistence";
 	String PACKAGE = "javax.persistence";
 	String PACKAGE_ = PACKAGE + '.';
 
